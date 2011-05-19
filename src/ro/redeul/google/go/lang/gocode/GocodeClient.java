a5582e43bc3cee10ea061e922f8de04524fe129c
Alexandre Normand <alexandre.normand@gmail.com>
2011-05-18 21:52:38 -0400

    Experimenting with gocode integration for autocompletion.

---
+++ src/ro/redeul/google/go/lang/gocode/GocodeClient.java
@@ -0,0 +1,11 @@
+package ro.redeul.google.go.lang.gocode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: skippyjon
+ * Date: 11-05-18
+ * Time: 8:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GocodeClient {
+}
