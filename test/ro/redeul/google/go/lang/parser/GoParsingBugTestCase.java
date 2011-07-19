dad5fa105d5b2cba1ed7e6cdc3bf0371e58c1758
Toader Mihai Claudiu <mtoader@gmail.com>
2011-07-19 17:28:37 +0300

    Fixed test cases.

---
+++ test/ro/redeul/google/go/lang/parser/GoParsingBugTestCase.java
@@ -0,0 +1,22 @@
+package ro.redeul.google.go.lang.parser;
+
+import java.io.File;
+
+/**
+ * Author: Toader Mihai Claudiu <mtoader@gmail.com>
+ * <p/>
+ * Date: 7/19/11
+ * Time: 5:15 PM
+ */
+public class GoParsingBugTestCase extends GoParsingTestCase {
+
+    @Override
+    protected String getBasePath() {
+        return super.getBasePath() + File.separator + "bugs";
+    }
+
+    public void testComment$first() throws Throwable {
+        doTest();
+    }
+
+}
