e02eb1b0531862a644f97a780d0a0766369e7103
Alexandre Normand <alexandre.normand@gmail.com>
2011-07-27 23:11:18 -0400

    Adding packaging and resources files for the go-ide, mac version

---
+++ go-ide/package_mac.sh
@@ -0,0 +1,25 @@
+GOSOURCEPATH=/Volumes/Bubble/projects/go
+IDEASOURCEPATH=/Volumes/Bubble/projects/idea
+PLUGINSOURCEPATH=/Volumes/Bubble/projects/google-go-lang-idea-plugin
+
+# Build the go sdk
+cd $GOSOURCEPATH/src
+#./all.bash
+cp -r $GOSOURCEPATH $IDEASOURCEPATH/build/conf/mac/go-sdk
+
+# Copy Go logo and about images
+cp $PLUGINSOURCEPATH/go-ide/resources/idea_community_about.png $IDEASOURCEPATH/community-resources/src/
+cp $PLUGINSOURCEPATH/go-ide/resources/idea_community_logo.png $IDEASOURCEPATH/community-resources/src/
+
+cd $IDEASOURCEPATH
+#ant build
+VERSION=$(cat build.txt)
+cp $IDEASOURCEPATH/out/artifacts/ideaIC-$VERSION.mac.zip $PLUGINSOURCEPATH/go-ide
+cd $PLUGINSOURCEPATH/go-ide
+rm -r Community\ Edition-IC-$VERSION.app
+unzip ideaIC-$VERSION.mac.zip
+
+unzip ../google-go-language.zip -d Community\ Edition-IC-$VERSION.app/plugins
+rm ideaIC-$VERSION.mac.zip
+
+echo "Now open up dmgCreator to make the dmg."
--- src/ro/redeul/google/go/util/GoUtil.java
+++ src/ro/redeul/google/go/util/GoUtil.java
@@ -1,6 +1,12 @@
 package ro.redeul.google.go.util;
 
+import com.intellij.ide.Bootstrap;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 /**
  * Author: Toader Mihai Claudiu <mtoader@gmail.com>
@@ -11,9 +17,47 @@ import java.io.File;
 public class GoUtil {
     public static final String ENV_GO_ROOT = "GOROOT";
 
-    public static String resolveGoogleGoHomePath() {
-        String goRoot = System.getenv(ENV_GO_ROOT);
+    /**
+     * Gets the idea home directory.
+     * Note: Copied over from IDEA Main class but the home directory doesn't seem to be properly made available to
+     * plugins.
+     * @return The idea home directory
+     * @throws IOException If error when building the path
+     */
+    private static File getIdeaHomeDir() throws IOException {
+        URL url = Bootstrap.class.getResource("");
+        if (url == null || !"jar".equals(url.getProtocol())) return null;
 
+        String path = url.getPath();
+
+        int start = path.indexOf("file:/");
+        int end = path.indexOf("!/");
+        if (start == -1 || end == -1) return null;
+
+        String jarFileUrl = path.substring(start, end);
+
+        try {
+            File bootstrapJar = new File(new URI(jarFileUrl));
+            return bootstrapJar.getParentFile().getParentFile();
+        } catch (URISyntaxException e) {
+            return null;
+        }
+    }
+
+    public static String resolveGoogleGoHomePath() {
+        // Try the bundled sdk first since it should have been tested as working, can still be overridden in the
+        // project settings
+        try {
+            String goRoot = getIdeaHomeDir().getCanonicalPath() + "/go-sdk";
+            if ( testPathExists(goRoot)) {
+                return goRoot;
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Impossible to get the idea home directory", e);
+        }
+
+        String goRoot = System.getenv(ENV_GO_ROOT);
         if ( testPathExists(goRoot) ) {
             return goRoot;
         }
