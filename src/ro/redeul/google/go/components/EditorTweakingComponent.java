43e1f3e9f4d5f0d5953c3327be19cdceba7f108c
Toader Mihai Claudiu <mtoader@gmail.com>
2011-08-15 19:06:42 +0300

    Closes #62. Register the last newline enter handler as an extension and not as a project bus watcher component.

--- src/ro/redeul/google/go/components/EditorTweakingComponent.java
+++ src/ro/redeul/google/go/components/EditorTweakingComponent.java
@@ -1,19 +1,14 @@
 package ro.redeul.google.go.components;
 
-import com.intellij.AppTopics;
-import com.intellij.codeInsight.documentation.DocumentationManager;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.CommandProcessor;
-import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.DocumentRunnable;
 import com.intellij.openapi.editor.ex.EditorSettingsExternalizable;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileDocumentManagerAdapter;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.messages.MessageBusConnection;
-import com.intellij.util.messages.Topic;
 import com.intellij.util.text.CharArrayUtil;
-import org.jetbrains.annotations.NotNull;
 import ro.redeul.google.go.GoFileType;
 
 /**
@@ -22,53 +17,33 @@ import ro.redeul.google.go.GoFileType;
  * Date: Sep 7, 2010
  * Time: 4:13:53 PM
  */
-public class EditorTweakingComponent implements ProjectComponent {
+public class EditorTweakingComponent extends FileDocumentManagerAdapter {
 
-    private Project project;
-    private MessageBusConnection connection;
+    @Override
+    public void beforeDocumentSaving(final Document document) {
 
-    public EditorTweakingComponent(Project project) {
-        this.project = project;
-    }
+        if (!document.isWritable())
+            return;
 
-    public void initComponent() {
-        // TODO: insert component initialization logic here
-    }
+        VirtualFile file = FileDocumentManager.getInstance().getFile(document);
+        if (file == null || file.getFileType() != GoFileType.GO_FILE_TYPE) {
+            return;
+        }
 
-    public void disposeComponent() {
-        // TODO: insert component disposal logic here
-    }
+        final EditorSettingsExternalizable settings = EditorSettingsExternalizable.getInstance();
+        if (settings != null && settings.isEnsureNewLineAtEOF()) {
+            return;
+        }
 
-    @NotNull
-    public String getComponentName() {
-        return "EditorTweakingComponent";
-    }
-
-    public void projectOpened() {
-
-        connection = project.getMessageBus().connect(project);
-
-        connection.subscribe(AppTopics.FILE_DOCUMENT_SYNC, new FileDocumentManagerAdapter() {
-            @Override
-            public void beforeDocumentSaving(final Document document) {
-
-                VirtualFile file = FileDocumentManager.getInstance().getFile(document);
-                if (file == null || file.getFileType() != GoFileType.GO_FILE_TYPE) {
-                    return;
-                }
-
-                final EditorSettingsExternalizable settings = EditorSettingsExternalizable.getInstance();
-                if ( settings != null && settings.isEnsureNewLineAtEOF() ) {
-                    return;
-                }
-
-                CommandProcessor.getInstance().runUndoTransparentAction(new Runnable() {
+        final int lines = document.getLineCount();
+        if (lines > 0) {
+            final int start = document.getLineStartOffset(lines - 1);
+            final int end = document.getLineEndOffset(lines - 1);
+            if (start != end) {
+                ApplicationManager.getApplication().runWriteAction(new DocumentRunnable(document, null) {
                     public void run() {
-                        final int lines = document.getLineCount();
-                        if (lines > 0) {
-                            int start = document.getLineStartOffset(lines - 1);
-                            int end = document.getLineEndOffset(lines - 1);
-                            if (start != end) {
+                        CommandProcessor.getInstance().runUndoTransparentAction(new Runnable() {
+                            public void run() {
                                 CharSequence content = document.getCharsSequence();
                                 if (CharArrayUtil.containsOnlyWhiteSpaces(content.subSequence(start, end))) {
                                     document.deleteString(start, end);
@@ -76,14 +51,10 @@ public class EditorTweakingComponent implements ProjectComponent {
                                     document.insertString(end, "\n");
                                 }
                             }
-                        }
+                        });
                     }
                 });
             }
-        });
-    }
-
-    public void projectClosed() {
-        connection.disconnect();
+        }
     }
 }
