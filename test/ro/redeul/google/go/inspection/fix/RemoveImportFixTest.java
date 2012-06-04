4ef84bc4e3797ab432fbe452ddc7cca9b4b10b1c
Leo Jay <python.leojay@gmail.com>
2012-06-04 22:29:17 +0800

    add test cases for `RemoveImportFix'

---
+++ test/ro/redeul/google/go/inspection/fix/RemoveImportFixTest.java
@@ -0,0 +1,45 @@
+package ro.redeul.google.go.inspection.fix;
+
+import com.intellij.codeInspection.InspectionManager;
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.DebugUtil;
+import ro.redeul.google.go.FileDataBasedTest;
+import ro.redeul.google.go.lang.psi.GoFile;
+import ro.redeul.google.go.lang.psi.toplevel.GoImportDeclaration;
+import ro.redeul.google.go.lang.psi.toplevel.GoImportDeclarations;
+
+public class RemoveImportFixTest extends FileDataBasedTest {
+    public void testSimple() throws Exception{ doTest(); }
+    public void testMultiLine1() throws Exception{ doTest(); }
+    public void testMultiLine2() throws Exception{ doTest(); }
+    public void testMultiLine3() throws Exception{ doTest(); }
+
+    @Override
+    protected void invoke(Project project, Editor editor, GoFile file) {
+        PsiElement element = file.findElementAt(editor.getSelectionModel().getSelectionStart());
+        if (!(element instanceof GoImportDeclaration)) {
+            element = element.getParent();
+        }
+
+        if (element instanceof GoImportDeclarations) {
+            element = ((GoImportDeclarations) element).getDeclarations()[0];
+        }
+
+        assertTrue(element instanceof GoImportDeclaration);
+        System.out.println(DebugUtil.psiToString(file, false, true));
+        InspectionManager im = InspectionManager.getInstance(project);
+        LocalQuickFix fix = null;
+        ProblemDescriptor pd = im.createProblemDescriptor(element, "", fix, ProblemHighlightType.ERROR, true);
+        new RemoveImportFix().applyFix(project, pd);
+    }
+
+    @Override
+    protected String getTestDataRelativePath() {
+        return "inspection/fix/removeImport/";
+    }
+}
