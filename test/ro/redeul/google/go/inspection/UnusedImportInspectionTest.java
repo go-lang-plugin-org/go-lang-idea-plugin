37486d36c074c3a533820a1fc7e7ecd37e0387f8
Liu Jing <liu.jing@devexperts.com>
2012-06-04 15:06:43 +0800

    add unit tests for class `UnusedImportInspection'

---
+++ test/ro/redeul/google/go/inspection/UnusedImportInspectionTest.java
@@ -0,0 +1,30 @@
+package ro.redeul.google.go.inspection;
+
+import com.intellij.codeInspection.InspectionManager;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.codeInspection.QuickFix;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import ro.redeul.google.go.FileDataBasedTest;
+import ro.redeul.google.go.lang.psi.GoFile;
+
+public class UnusedImportInspectionTest extends FileDataBasedTest {
+    public void testSimple() throws Exception{ doTest(); }
+    public void testOnlyOneImport() throws Exception{ doTest(); }
+
+    @Override
+    protected void invoke(Project project, Editor myEditor, PsiFile file) {
+        InspectionManager im = InspectionManager.getInstance(project);
+        for (ProblemDescriptor pd : new UnusedImportInspection().doCheckFile((GoFile) file, im)) {
+            QuickFix[] fixes = pd.getFixes();
+            assertEquals(1, fixes.length);
+            fixes[0].applyFix(project, pd);
+        }
+    }
+
+    @Override
+    protected String getTestDataRelativePath() {
+        return "inspection/unusedImport/";
+    }
+}
