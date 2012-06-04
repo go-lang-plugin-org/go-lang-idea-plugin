eca2954090c436add82c8e5639189f66ce5f8c20
Leo Jay <python.leojay@gmail.com>
2012-06-04 19:47:57 +0800

    add class `RemoveVariableFix' which will be used to remove unused variables/constants.

---
+++ src/ro/redeul/google/go/inspection/fix/RemoveVariableFix.java
@@ -0,0 +1,114 @@
+package ro.redeul.google.go.inspection.fix;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiWhiteSpace;
+import org.jetbrains.annotations.NotNull;
+import ro.redeul.google.go.lang.lexer.GoTokenTypes;
+import ro.redeul.google.go.lang.psi.declarations.GoConstDeclaration;
+import ro.redeul.google.go.lang.psi.declarations.GoConstDeclarations;
+import ro.redeul.google.go.lang.psi.declarations.GoVarDeclaration;
+import ro.redeul.google.go.lang.psi.declarations.GoVarDeclarations;
+import ro.redeul.google.go.lang.psi.expressions.GoExpr;
+import ro.redeul.google.go.lang.psi.expressions.literals.GoIdentifier;
+
+import static ro.redeul.google.go.lang.psi.utils.GoPsiUtils.isNodeOfType;
+
+public class RemoveVariableFix implements LocalQuickFix {
+    @NotNull
+    @Override
+    public String getName() {
+        return "Remove unused import";
+    }
+
+    @NotNull
+    @Override
+    public String getFamilyName() {
+        return "Import";
+    }
+
+    @Override
+    public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+        PsiElement element = descriptor.getPsiElement();
+        if (!(element instanceof GoIdentifier)) {
+            return;
+        }
+
+        GoIdentifier id = (GoIdentifier) element;
+        PsiElement parent = id.getParent();
+        if (parent instanceof GoVarDeclaration) {
+            GoVarDeclaration gsvd = (GoVarDeclaration) parent;
+            removeIdentifier(id, parent, gsvd.getIdentifiers(), gsvd.getExpressions());
+        } else if (parent instanceof GoConstDeclaration) {
+            GoConstDeclaration gcd = (GoConstDeclaration) parent;
+            removeIdentifier(id, parent, gcd.getIdentifiers(), gcd.getExpressions());
+        }
+    }
+
+    private void removeIdentifier(GoIdentifier id, PsiElement parent, GoIdentifier[] ids, GoExpr[] exprs) {
+        boolean idWithDummy = ids.length == 2 && (ids[0].isBlank() && ids[1].isEquivalentTo(id) ||
+                                        ids[1].isBlank() && ids[0].isEquivalentTo(id));
+        if (ids.length == 1 || idWithDummy) {
+            removeWholeElement(parent);
+            return;
+        }
+
+        int index = identifierIndex(ids, id);
+        if (index < 0) {
+            return;
+        }
+
+        if (index == ids.length - 1) {
+            parent.deleteChildRange(ids[index - 1].getNextSibling(), ids[index]);
+        } else {
+            parent.deleteChildRange(ids[index], ids[index + 1].getPrevSibling());
+        }
+
+        if (exprs.length == ids.length) {
+            if (index == ids.length - 1) {
+                parent.deleteChildRange(exprs[index - 1].getNextSibling(), exprs[index]);
+            } else {
+                parent.deleteChildRange(exprs[index], exprs[index + 1].getPrevSibling());
+            }
+        }
+    }
+
+    private static int identifierIndex(GoIdentifier[] ids, GoIdentifier id) {
+        for (int i = 0; i < ids.length; i++) {
+            if (ids[i].isEquivalentTo(id)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static void removeWholeElement(PsiElement element) {
+        if (isOnlyVarDeclaration(element) || isOnlyConstDeclaration(element)) {
+            element = element.getParent();
+        }
+
+        PsiElement prev = element.getPrevSibling();
+        if (prev instanceof PsiWhiteSpace) {
+            prev.delete();
+        }
+
+        PsiElement next = element.getNextSibling();
+        if (next != null && isNodeOfType(next, GoTokenTypes.wsNLS)) {
+            next.delete();
+        }
+
+        element.delete();
+    }
+
+    private static boolean isOnlyConstDeclaration(PsiElement e) {
+        return e instanceof GoConstDeclaration && e.getParent() instanceof GoConstDeclarations &&
+               ((GoConstDeclarations) e.getParent()).getDeclarations().length == 1;
+    }
+
+    private static boolean isOnlyVarDeclaration(PsiElement e) {
+        return e instanceof GoVarDeclaration && e.getParent() instanceof GoVarDeclarations &&
+               ((GoVarDeclarations) e.getParent()).getDeclarations().length == 1;
+    }
+}
---
+++ test/ro/redeul/google/go/inspection/fix/RemoveVariableFixTest.java
@@ -0,0 +1,55 @@
+package ro.redeul.google.go.inspection.fix;
+
+import com.intellij.codeInspection.InspectionManager;
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.DebugUtil;
+import ro.redeul.google.go.FileDataBasedTest;
+import ro.redeul.google.go.lang.psi.GoFile;
+import ro.redeul.google.go.lang.psi.expressions.literals.GoIdentifier;
+
+public class RemoveVariableFixTest extends FileDataBasedTest {
+    public void testSimple() throws Exception{ doTest(); }
+    public void testShortVarAndDummy1() throws Exception{ doTest(); }
+    public void testShortVarAndDummy2() throws Exception{ doTest(); }
+    public void testShortOnlyOneVar1() throws Exception{ doTest(); }
+    public void testShortOnlyOneVar2() throws Exception{ doTest(); }
+
+    public void testVarAndDummy1() throws Exception{ doTest(); }
+    public void testVarAndDummy2() throws Exception{ doTest(); }
+    public void testOnlyOneVar1() throws Exception{ doTest(); }
+    public void testOnlyOneVar2() throws Exception{ doTest(); }
+    public void testOnlyOneVarWithType1() throws Exception{ doTest(); }
+    public void testOnlyOneVarWithType2() throws Exception{ doTest(); }
+
+    public void testMultiLine1() throws Exception{ doTest(); }
+    public void testMultiLine2() throws Exception{ doTest(); }
+
+    public void testConstSimple() throws Exception{ doTest(); }
+    public void testConstMultiLine1() throws Exception{ doTest(); }
+    public void testConstMultiLine2() throws Exception{ doTest(); }
+
+
+    @Override
+    protected void invoke(Project project, Editor editor, GoFile file) {
+        PsiElement element = file.findElementAt(editor.getSelectionModel().getSelectionStart());
+        if (!(element instanceof GoIdentifier)) {
+            element = element.getParent();
+        }
+        assertTrue(element instanceof GoIdentifier);
+        System.out.println(DebugUtil.psiToString(file, false, true));
+        InspectionManager im = InspectionManager.getInstance(project);
+        LocalQuickFix fix = null;
+        ProblemDescriptor pd = im.createProblemDescriptor(element, "", fix, ProblemHighlightType.ERROR, true);
+        new RemoveVariableFix().applyFix(project, pd);
+    }
+
+    @Override
+    protected String getTestDataRelativePath() {
+        return "inspection/fix/removeVariable/";
+    }
+}
