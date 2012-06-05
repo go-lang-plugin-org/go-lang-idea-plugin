fbb4f542dc19a29fe0e53ebabf7c27f703f5f502
Mihai Claudiu Toader <mtoader@gmail.com>
2012-06-05 18:35:21 +0200

    Fix GoConstDeclarationImpl.getExpressions() to return the proper objects.
     Signed-off-by: Mihai Claudiu Toader <mtoader@gmail.com>

--- src/ro/redeul/google/go/lang/psi/impl/declarations/GoConstDeclarationImpl.java
+++ src/ro/redeul/google/go/lang/psi/impl/declarations/GoConstDeclarationImpl.java
@@ -1,22 +1,22 @@
 package ro.redeul.google.go.lang.psi.impl.declarations;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.ResolveState;
 import com.intellij.psi.scope.PsiScopeProcessor;
+import com.intellij.util.ReflectionCache;
 import org.jetbrains.annotations.NotNull;
+import ro.redeul.google.go.lang.parser.GoElementTypes;
 import ro.redeul.google.go.lang.psi.declarations.GoConstDeclaration;
 import ro.redeul.google.go.lang.psi.expressions.GoExpr;
 import ro.redeul.google.go.lang.psi.expressions.literals.GoIdentifier;
 import ro.redeul.google.go.lang.psi.impl.GoPsiElementBase;
 import ro.redeul.google.go.lang.psi.visitors.GoElementVisitor;
 
-/**
- * Author: Toader Mihai Claudiu <mtoader@gmail.com>
- * <p/>
- * Date: 7/16/11
- * Time: 4:10 AM
- */
 public class GoConstDeclarationImpl extends GoPsiElementBase implements GoConstDeclaration {
 
     public GoConstDeclarationImpl(@NotNull ASTNode node) {
@@ -31,7 +31,18 @@ public class GoConstDeclarationImpl extends GoPsiElementBase implements GoConstD
     @Override
     @NotNull
     public GoExpr[] getExpressions() {
-        return findChildrenByClass(GoExpr.class);
+        PsiElement list =
+            findChildByType(GoElementTypes.EXPRESSION_LIST);
+
+        if (list == null) {
+            return new GoExpr[0];
+        }
+
+        List<GoExpr> result = new ArrayList<GoExpr>();
+        for (PsiElement cur = list.getFirstChild(); cur != null; cur = cur.getNextSibling()) {
+            if (ReflectionCache.isInstance(cur, GoExpr.class)) result.add((GoExpr)cur);
+        }
+        return result.toArray((GoExpr[]) Array.newInstance(GoExpr.class, result.size()));
     }
 
     @Override
