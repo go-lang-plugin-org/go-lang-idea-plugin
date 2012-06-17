43fbec12faf497baca0465b6dfcf5ed12c460ecc
Mihai Claudiu Toader <mtoader@gmail.com>
2012-06-17 03:02:09 +0200

    Added test case for getCondition on a for clauses stmt.
    Signed-off-by: Mihai Claudiu Toader <mtoader@gmail.com>

---
+++ test/ro/redeul/google/go/psi/GoPsiForStatementTest.java
@@ -0,0 +1,70 @@
+package ro.redeul.google.go.psi;
+
+import ro.redeul.google.go.lang.psi.GoFile;
+import ro.redeul.google.go.lang.psi.expressions.GoLiteralExpression;
+import ro.redeul.google.go.lang.psi.expressions.binary.GoAdditiveExpression;
+import ro.redeul.google.go.lang.psi.statements.GoForWithClausesStatement;
+import static ro.redeul.google.go.util.GoPsiTestUtils.castAs;
+import static ro.redeul.google.go.util.GoPsiTestUtils.childAt;
+import static ro.redeul.google.go.util.GoPsiTestUtils.get;
+import static ro.redeul.google.go.util.GoPsiTestUtils.getAs;
+
+public class GoPsiForStatementTest extends GoPsiTestCase {
+
+    public void testForWithClauseNullCondition() throws Exception {
+        GoFile file = get(parse("" +
+                                    "package main;\n" +
+                                    "func test() {\n" +
+                                    "   for ;; {}\n" +
+                                    "}"));
+
+        GoForWithClausesStatement forStmt =
+            castAs(GoForWithClausesStatement.class, 0,
+                   get(
+                       childAt(0,
+                               file.getFunctions()
+                       ).getBlock()
+                   ).getStatements()
+            );
+
+        assertNull(forStmt.getCondition());
+    }
+
+    public void testForWithClauseNotNullCondition() throws Exception {
+        GoFile file = get(parse("" +
+                                    "package main;\n" +
+                                    "func test() {\n" +
+                                    "   for ;e; {}\n" +
+                                    "}"));
+
+        GoForWithClausesStatement forStmt =
+            castAs(GoForWithClausesStatement.class, 0,
+                   get(
+                       childAt(0,
+                               file.getFunctions()
+                       ).getBlock()
+                   ).getStatements()
+            );
+
+        getAs(GoLiteralExpression.class, forStmt.getCondition());
+    }
+
+    public void testForWithClauseNotNullConditionAdditive() throws Exception {
+        GoFile file = get(parse("" +
+                                    "package main;\n" +
+                                    "func test() {\n" +
+                                    "   for ;e+f; {}\n" +
+                                    "}"));
+
+        GoForWithClausesStatement forStmt =
+            castAs(GoForWithClausesStatement.class, 0,
+                   get(
+                       childAt(0,
+                               file.getFunctions()
+                       ).getBlock()
+                   ).getStatements()
+            );
+
+        getAs(GoAdditiveExpression.class, forStmt.getCondition());
+    }
+}
