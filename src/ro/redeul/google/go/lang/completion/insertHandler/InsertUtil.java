c4a705e09025853ca9fe88cd83382784a56dfe6d
Leo Jay <python.leojay@gmail.com>
2012-06-24 23:48:40 +0800

    When inserting parentheses, respect the indention settings.

--- src/ro/redeul/google/go/lang/completion/insertHandler/InsertUtil.java
+++ src/ro/redeul/google/go/lang/completion/insertHandler/InsertUtil.java
@@ -2,10 +2,12 @@ package ro.redeul.google.go.lang.completion.insertHandler;
 
 import com.intellij.codeInsight.completion.InsertionContext;
 
+import static ro.redeul.google.go.util.EditorUtil.pressEnterAtLineEnd;
+
 class InsertUtil {
     public static void insertParenthesis(InsertionContext context) {
         int offset = context.getTailOffset();
-        context.getDocument().insertString(offset, "(\n    \n)\n");
-        context.getEditor().getCaretModel().moveToOffset(offset + 6);
+        context.getDocument().insertString(offset, "(\n)\n");
+        pressEnterAtLineEnd(context.getEditor());
     }
 }
