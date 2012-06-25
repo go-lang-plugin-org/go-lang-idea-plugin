abf6037a8685eaa8c504db8bdfc726d24366f003
Mihai Claudiu Toader <mtoader@gmail.com>
2012-06-25 12:20:29 +0200

    Fix RelationalExpression.getOperator() returning null.
     Signed-off-by: Mihai Claudiu Toader <mtoader@gmail.com>

--- src/ro/redeul/google/go/lang/psi/impl/expressions/binary/GoRelationalExpressionImpl.java
+++ src/ro/redeul/google/go/lang/psi/impl/expressions/binary/GoRelationalExpressionImpl.java
@@ -1,7 +1,10 @@
 package ro.redeul.google.go.lang.psi.impl.expressions.binary;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NotNull;
+import ro.redeul.google.go.lang.parser.GoElementTypes;
 import ro.redeul.google.go.lang.psi.expressions.GoExpr;
 import ro.redeul.google.go.lang.psi.expressions.binary.GoRelationalExpression;
 import ro.redeul.google.go.lang.psi.types.GoType;
@@ -18,5 +21,12 @@ public class GoRelationalExpressionImpl extends GoBinaryExpressionImpl
         GoExpr operand = getLeftOperand();
         return operand != null ? operand.getType() : null;
     }
+
+    @Override
+    public IElementType getOperator() {
+        PsiElement child = findChildByFilter(GoElementTypes.RELATIONAL_OPS);
+        return child != null ? child.getNode().getElementType(): null;
+    }
+
 }
 
