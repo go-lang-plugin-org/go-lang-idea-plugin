2ff22c02c451179c6e1d699e583aed688f881c4b
Leo Jay <python.leojay@gmail.com>
2012-06-27 18:05:03 +0800

    Add quote handler. So that when user checked "Insert pair quote" in settings, quotation marks are always inserted in a pair.

---
+++ src/ro/redeul/google/go/codeInsight/editorActions/GoQuoteHandler.java
@@ -0,0 +1,23 @@
+package ro.redeul.google.go.codeInsight.editorActions;
+
+import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.highlighter.HighlighterIterator;
+import ro.redeul.google.go.lang.lexer.GoTokenTypeSets;
+import ro.redeul.google.go.lang.lexer.GoTokenTypes;
+
+public class GoQuoteHandler extends SimpleTokenSetQuoteHandler {
+    public GoQuoteHandler() {
+        super(GoTokenTypeSets.STRINGS);
+    }
+
+    @Override
+    public boolean isOpeningQuote(HighlighterIterator iterator, int offset) {
+        return iterator.getTokenType() == GoTokenTypes.mWRONG;
+    }
+
+    @Override
+    public boolean hasNonClosedLiteral(Editor editor, HighlighterIterator iterator, int offset) {
+        return true;
+    }
+}
