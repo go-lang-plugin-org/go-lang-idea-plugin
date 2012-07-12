e4b42f7dd88eb5ee30a42707ae96a47ca8ccdcaf
Mihai Claudiu Toader <mtoader@gmail.com>
2012-07-13 01:59:21 +0200

    Made GoVarDeclaration a Statement.
     Signed-off-by: Mihai Claudiu Toader <mtoader@gmail.com>

--- src/ro/redeul/google/go/lang/psi/declarations/GoVarDeclaration.java
+++ src/ro/redeul/google/go/lang/psi/declarations/GoVarDeclaration.java
@@ -4,9 +4,10 @@ import org.jetbrains.annotations.Nullable;
 import ro.redeul.google.go.lang.psi.GoPsiElement;
 import ro.redeul.google.go.lang.psi.expressions.GoExpr;
 import ro.redeul.google.go.lang.psi.expressions.literals.GoLiteralIdentifier;
+import ro.redeul.google.go.lang.psi.statements.GoStatement;
 import ro.redeul.google.go.lang.psi.types.GoType;
 
-public interface GoVarDeclaration extends GoPsiElement {
+public interface GoVarDeclaration extends GoPsiElement, GoStatement {
 
     GoLiteralIdentifier[] getIdentifiers();
 
