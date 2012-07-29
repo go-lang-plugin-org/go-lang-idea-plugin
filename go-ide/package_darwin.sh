709bbe6b08e3c610b9eaf2863c99dd5af4763b77
Mihai Claudiu Toader <mtoader@gmail.com>
2012-07-29 07:09:43 +0200

    Prepare GoIde building scripts for MACOSX.
    Signed-off-by: Mihai Claudiu Toader <mtoader@gmail.com>

--- go-ide/package_darwin.sh
+++ go-ide/package_darwin.sh
@@ -2,23 +2,24 @@
 #
 # Author: Alexandre Normand (https://github.com/alexandre-normand)
 # Date: July 30th, 2011
-# 
+#
 # Many variables (including source paths) can/must be overriden.
 # Refer to package.sh for the list
 
 SOURCE_PATH_GO_PLUGIN=${SOURCE_PATH_GO_PLUGIN:-`pwd`/..}
 TARGET_HOST=darwin ./package.sh
+DIST_FOLDER=${DIST_FOLDER:-${SOURCE_PATH_GO_PLUGIN}/../dist}
 
 function createDmg()
 {
     echo
-    echo "Creating dmg image from ${SOURCE_PATH_GO_PLUGIN}/dist/darwin_$1"
-    echo 
+    echo "Creating dmg image from ${DIST_FOLDER}/darwin_$1"
+    echo
 
-    rm -r ${SOURCE_PATH_GO_PLUGIN}/dist/darwin_$1
-    unzip -o ${SOURCE_PATH_GO_PLUGIN}/dist/goide-darwin_$1.zip -d ${SOURCE_PATH_GO_PLUGIN}/dist/darwin_$1
-    mv -f "${SOURCE_PATH_GO_PLUGIN}/dist/darwin_$1/go-ide" "${SOURCE_PATH_GO_PLUGIN}/dist/darwin_$1/Go Ide.app"
-    SRC_FOLDER="${SOURCE_PATH_GO_PLUGIN}/dist/darwin_$1" IMAGE_FILE=${SOURCE_PATH_GO_PLUGIN}/dist/goide_osx_$1.dmg ./make_dmg.sh
+    rm -r "${DIST_FOLDER}/darwin_$1"
+    unzip -o "${DIST_FOLDER}/goide-darwin_$1.zip" -d "${DIST_FOLDER}/darwin_$1"
+    mv -f "${DIST_FOLDER}/darwin_$1/go-ide" "${DIST_FOLDER}/darwin_$1/Go Ide.app"
+    SRC_FOLDER="${DIST_FOLDER}/darwin_$1" IMAGE_FILE=${DIST_FOLDER}/goide_osx_$1.dmg ./make_dmg.sh
 }
 
 createDmg 386
