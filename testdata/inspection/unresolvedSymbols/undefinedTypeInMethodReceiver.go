424c0611829ba9cc9aaf7bfbcdecaad058809374
Leo Jay <python.leojay@gmail.com>
2012-09-09 18:11:16 +0800

    Update test cases and release notes.

--- testdata/inspection/unresolvedSymbols/undefinedTypeInMethodReceiver.go
+++ testdata/inspection/unresolvedSymbols/undefinedTypeInMethodReceiver.go
@@ -1,6 +1,6 @@
 package main
 
-func (a */*begin*/A/*end.Unresolved symbol: 'A'*/)main() {
+func (a */*begin*/A/*end.Unresolved symbol: 'A'|CreateTypeFix*/)main() {
     arr := nil
     for j, _ := range arr {
     }
