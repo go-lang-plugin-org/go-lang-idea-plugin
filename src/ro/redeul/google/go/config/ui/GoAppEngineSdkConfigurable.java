7cf9894e2e7f08fca2e9295c0f40b416fc522bd0
Mihai Claudiu Toader <mtoader@gmail.com>
2012-12-13 00:14:07 -0800

    Avoid a NPE when looking at an old AppEngine SDK.
    Refs #175
    
    Signed-off-by: Mihai Claudiu Toader <mtoader@gmail.com>

--- src/ro/redeul/google/go/config/ui/GoAppEngineSdkConfigurable.java
+++ src/ro/redeul/google/go/config/ui/GoAppEngineSdkConfigurable.java
@@ -2,6 +2,8 @@ package ro.redeul.google.go.config.ui;
 
 import javax.swing.*;
 
+import static java.lang.String.format;
+
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.projectRoots.AdditionalDataConfigurable;
 import com.intellij.openapi.projectRoots.Sdk;
@@ -55,19 +57,22 @@ public class GoAppEngineSdkConfigurable implements AdditionalDataConfigurable {
     @Override
     public void reset() {
         SdkAdditionalData data = sdk.getSdkAdditionalData();
-        if ( ! (data instanceof GoAppEngineSdkData)) {
+        if (!(data instanceof GoAppEngineSdkData)) {
             return;
         }
 
         GoAppEngineSdkData sdkData = (GoAppEngineSdkData) data;
 
         labelVersion.setText(sdkData.VERSION_MAJOR);
-        labelTargetSystem.setText(String.format("%s-%s",
-                sdkData.TARGET_OS.getName(), sdkData.TARGET_ARCH.getName()
-        ));
-
         labelTimestamp.setText(sdkData.VERSION_MINOR);
         labelAppLevel.setText(sdkData.API_VERSIONS);
+
+        if (sdkData.TARGET_OS != null && sdkData.TARGET_ARCH != null)
+            labelTargetSystem.setText(
+                format("%s-%s",
+                       sdkData.TARGET_OS.getName(),
+                       sdkData.TARGET_ARCH.getName()
+                ));
     }
 
     @Override
