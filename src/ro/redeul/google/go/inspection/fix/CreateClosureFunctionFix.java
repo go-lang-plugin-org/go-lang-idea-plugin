9885e2e53c6ffefeee23331c789521998fac6d5c
José Santos <henrique_1609@me.com>
2013-12-05 20:29:03 +0000

    Added a new inspection fix to generate a closure. And updated the Function fix to generate the arguments Sorry my bad english :D

--- src/ro/redeul/google/go/inspection/fix/CreateClosureFunctionFix.java
+++ src/ro/redeul/google/go/inspection/fix/CreateClosureFunctionFix.java
@@ -10,28 +10,31 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import ro.redeul.google.go.lang.psi.GoPsiElement;
+import ro.redeul.google.go.lang.psi.declarations.GoVarDeclaration;
+import ro.redeul.google.go.lang.psi.declarations.GoVarDeclarations;
 import ro.redeul.google.go.lang.psi.expressions.literals.GoLiteral;
-import ro.redeul.google.go.lang.psi.expressions.literals.GoLiteralFunction;
 import ro.redeul.google.go.lang.psi.expressions.literals.GoLiteralIdentifier;
 import ro.redeul.google.go.lang.psi.expressions.primary.GoCallOrConvExpression;
 import ro.redeul.google.go.lang.psi.expressions.primary.GoLiteralExpression;
+import ro.redeul.google.go.lang.psi.statements.GoBlockStatement;
+import ro.redeul.google.go.lang.psi.statements.GoShortVarDeclaration;
 import ro.redeul.google.go.lang.psi.toplevel.GoFunctionDeclaration;
 import ro.redeul.google.go.util.GoUtil;
 
-import static ro.redeul.google.go.lang.psi.utils.GoPsiUtils.findParentOfType;
-import static ro.redeul.google.go.lang.psi.utils.GoPsiUtils.psiIsA;
+import static ro.redeul.google.go.lang.psi.utils.GoPsiUtils.*;
 import static ro.redeul.google.go.util.EditorUtil.pressEnter;
 import static ro.redeul.google.go.util.EditorUtil.reformatLines;
 
-public class CreateFunctionFix extends LocalQuickFixAndIntentionActionOnPsiElement {
-    public CreateFunctionFix(@Nullable PsiElement element) {
+public class CreateClosureFunctionFix extends LocalQuickFixAndIntentionActionOnPsiElement {
+    public CreateClosureFunctionFix(@Nullable PsiElement element) {
         super(element);
     }
 
     @NotNull
     @Override
     public String getText() {
-        return "Create function \"" + getStartElement().getText() + "\"";
+        return "Create closure function \"" + getStartElement().getText() + "\"";
     }
 
     @NotNull
@@ -40,40 +43,46 @@ public class CreateFunctionFix extends LocalQuickFixAndIntentionActionOnPsiEleme
         return "Variable Declaration";
     }
 
-
     @Override
     public void invoke(@NotNull Project project,
                        @NotNull final PsiFile file,
                        @Nullable("is null when called from inspection") final Editor editor,
                        @NotNull PsiElement startElement, @NotNull PsiElement endElement) {
+
         final PsiElement e = startElement;
-        if (!e.getContainingFile().equals(file) || !isFunctionNameIdentifier(e)) {
-            return;
-        }
+        final PsiElement p = findChildOfClass(findParentOfType(e, GoFunctionDeclaration.class), GoBlockStatement.class);
 
-        GoFunctionDeclaration fd = findParentOfType(e, GoFunctionDeclaration.class);
-        while (fd instanceof GoLiteralFunction) {
-            fd = findParentOfType(fd.getParent(), GoFunctionDeclaration.class);
-        }
+        GoPsiElement childOfClass = findChildOfClass(p, GoVarDeclarations.class);
+        if (childOfClass == null)
+            childOfClass = findChildOfClass(p, GoVarDeclaration.class);
+        if (childOfClass == null)
+            childOfClass = findChildOfClass(p, GoShortVarDeclaration.class);
 
+        final String fnArguments;
         //put arguments with the new func
-
-
-        final String fnArguments = GoUtil.InspectionGenFuncArgs(e);
+        StringBuilder stringBuilder = new StringBuilder();
+        if (isFunctionNameIdentifier(e)) {
+            fnArguments = GoUtil.InspectionGenFuncArgs(e);
+        } else {
+            fnArguments = "";
+        }
         //end arguments
 
         final int insertPoint;
-        if (fd != null) {
-            insertPoint = fd.getTextRange().getEndOffset();
+
+        if (childOfClass == null) {
+            insertPoint = p.getTextOffset() + 2;
         } else {
-            insertPoint = file.getTextRange().getEndOffset();
+            insertPoint = childOfClass.getTextRange().getEndOffset();
         }
+
 
         ApplicationManager.getApplication().runWriteAction(new Runnable() {
             @Override
             public void run() {
                 Document doc = PsiDocumentManager.getInstance(e.getProject()).getDocument(file);
-                doc.insertString(insertPoint, String.format("\n\nfunc %s(%s) {\n}\n", e.getText(), fnArguments));
+
+                doc.insertString(insertPoint, String.format("\n\n%s := func (%s) {\n}\n", e.getText(), fnArguments));
                 if (editor != null) {
                     int line = doc.getLineNumber(insertPoint);
                     int offset = doc.getLineEndOffset(line + 2);
--- src/ro/redeul/google/go/util/GoUtil.java
+++ src/ro/redeul/google/go/util/GoUtil.java
@@ -2,6 +2,18 @@ package ro.redeul.google.go.util;
 
 import com.intellij.ide.Bootstrap;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import ro.redeul.google.go.lang.psi.expressions.GoExpr;
+import ro.redeul.google.go.lang.psi.expressions.literals.GoLiteral;
+import ro.redeul.google.go.lang.psi.expressions.literals.GoLiteralFunction;
+import ro.redeul.google.go.lang.psi.expressions.literals.GoLiteralIdentifier;
+import ro.redeul.google.go.lang.psi.expressions.literals.composite.GoLiteralComposite;
+import ro.redeul.google.go.lang.psi.expressions.primary.GoCallOrConvExpression;
+import ro.redeul.google.go.lang.psi.toplevel.GoFunctionParameter;
+import ro.redeul.google.go.lang.psi.typing.GoType;
+import ro.redeul.google.go.lang.psi.typing.GoTypeName;
+import ro.redeul.google.go.lang.psi.typing.GoTypePointer;
+import ro.redeul.google.go.lang.psi.typing.GoTypeSlice;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,6 +35,7 @@ public class GoUtil {
      * Gets the idea home directory.
      * Note: Copied over from IDEA Main class but the home directory doesn't seem to be properly made available to
      * plugins.
+     *
      * @return The idea home directory
      */
     private static File getIdeaHomeDir() {
@@ -47,21 +60,20 @@ public class GoUtil {
 
     public static boolean testGoHomeFolder(String goRoot) {
         return goRoot != null
-            && goRoot.trim().length() > 0 && new File(goRoot).isDirectory();
+                && goRoot.trim().length() > 0 && new File(goRoot).isDirectory();
     }
 
     private final static Pattern RE_PACKAGE_TARGET =
-        Pattern.compile("^TARG=([^\\s]+)\\s*$", Pattern.MULTILINE);
+            Pattern.compile("^TARG=([^\\s]+)\\s*$", Pattern.MULTILINE);
 
     /**
      * Returns a string if there is a TARG=xxx specified in the provided makefile and null if there is no such file.
      *
      * @param makefile the file we want to test (can be null)
-     *
      * @return the specified target or null
      */
     public static String getTargetFromMakefile(VirtualFile makefile) {
-        if ( makefile == null ) {
+        if (makefile == null) {
             return null;
         }
 
@@ -69,7 +81,7 @@ public class GoUtil {
             String content = new String(makefile.contentsToByteArray(), "UTF-8");
 
             Matcher matcher = RE_PACKAGE_TARGET.matcher(content);
-            if ( matcher.find() ) {
+            if (matcher.find()) {
                 return matcher.group(1);
             }
         } catch (IOException e) {
@@ -77,6 +89,88 @@ public class GoUtil {
         }
 
         return null;
+    }
+
+
+    public static String getFuncDecParam(GoLiteralFunction goLiteralFunction) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("func(");
+
+        int counter = 0;
+        for (GoFunctionParameter parameter :
+                goLiteralFunction.getParameters()) {
+            if (counter != 0)
+                stringBuilder.append(',');
+            stringBuilder.append(parameter.getType().getText());
+            counter++;
+        }
+        stringBuilder.append(')');
+        counter = 0;
+        for (GoFunctionParameter parameter :
+                goLiteralFunction.getResults()) {
+            if (counter == 0) {
+                stringBuilder.append('(');
+            } else {
+                stringBuilder.append(',');
+            }
+            stringBuilder.append(parameter.getType().getText());
+            counter++;
+        }
+
+        if (counter != 0)
+            stringBuilder.append(')');
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Helper method to generate function arguments type, based on param being passed to the function
+     *
+     * @param e -> parent -> GoCallOrConvExpression
+     * @return the generated arugment list ex: arg0 int, arg1 string
+     */
+    public static String InspectionGenFuncArgs(PsiElement e) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int arg = 0;
+        for (GoExpr argument : ((GoCallOrConvExpression) e.getParent()).getArguments()) {
+            if (arg != 0)
+                stringBuilder.append(',');
+            stringBuilder.append("arg").append(arg).append(" ");
+            PsiElement firstChildExp = argument.getFirstChild();
+            //Compositives Types
+            if (firstChildExp instanceof GoLiteralIdentifier) {
+                stringBuilder.append(((GoLiteral) firstChildExp).getType().name().toLowerCase());
+            } else if (firstChildExp instanceof GoLiteralComposite || firstChildExp.getText().equals("&")) {
+
+                GoType[] goTypes = argument.getType();
+                if (goTypes.length > 0) {
+                    GoType goType = goTypes[0];
+                    if (goType instanceof GoTypeSlice) {
+                        stringBuilder.append(((GoTypeSlice) goType).getPsiType().getText());
+                    } else if (goType instanceof GoTypeName) {
+                        if (firstChildExp.getText().equals("&"))
+                            stringBuilder.append('*');
+                        stringBuilder.append(((GoTypeName) goType).getPsiType().getText());
+
+                    } else if (goType instanceof GoTypePointer) {
+                        goType = ((GoTypePointer) goType).getTargetType();
+                        stringBuilder.append('*');
+                        stringBuilder.append(((GoTypeName) goType).getPsiType().getText());
+                    }
+                } else {
+                    stringBuilder.append("interface{}");
+                }
+
+            } else if (firstChildExp instanceof GoLiteralFunction) {
+                stringBuilder.append(GoUtil.getFuncDecParam((GoLiteralFunction) firstChildExp));
+            } else {
+                stringBuilder.append(((GoLiteral) firstChildExp).getType().name().toLowerCase());
+            }
+            arg++;
+        }
+
+        return stringBuilder.toString();
     }
 }
 
