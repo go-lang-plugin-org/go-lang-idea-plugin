281ee732b5632198c1aa464d1125ff42e328b00f
José Santos <henrique_1609@me.com>
2013-12-07 21:49:23 +0000

    CreateFunctionFix in nested packages

--- src/ro/redeul/google/go/inspection/fix/CreateFunctionFix.java
+++ src/ro/redeul/google/go/inspection/fix/CreateFunctionFix.java
@@ -4,12 +4,14 @@ import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import ro.redeul.google.go.lang.psi.GoFile;
 import ro.redeul.google.go.lang.psi.expressions.literals.GoLiteralFunction;
 import ro.redeul.google.go.lang.psi.toplevel.GoFunctionDeclaration;
 import ro.redeul.google.go.util.GoUtil;
@@ -19,8 +21,21 @@ import static ro.redeul.google.go.util.EditorUtil.pressEnter;
 import static ro.redeul.google.go.util.EditorUtil.reformatLines;
 
 public class CreateFunctionFix extends LocalQuickFixAndIntentionActionOnPsiElement {
+
+    private PsiFile workingFile = null;
+
     public CreateFunctionFix(@Nullable PsiElement element) {
         super(element);
+    }
+
+
+    public CreateFunctionFix(PsiElement element, PsiElement resolve) {
+        super(element);
+    }
+
+    public CreateFunctionFix(PsiElement element, PsiFile workingFile) {
+        super(element);
+        this.workingFile = workingFile;
     }
 
     @NotNull
@@ -37,38 +52,57 @@ public class CreateFunctionFix extends LocalQuickFixAndIntentionActionOnPsiEleme
 
 
     @Override
-    public void invoke(@NotNull Project project,
-                       @NotNull final PsiFile file,
-                       @Nullable("is null when called from inspection") final Editor editor,
-                       @NotNull PsiElement startElement, @NotNull PsiElement endElement) {
-        final PsiElement e = startElement;
-
-
-        GoFunctionDeclaration fd = findParentOfType(e, GoFunctionDeclaration.class);
-        while (fd instanceof GoLiteralFunction) {
-            fd = findParentOfType(fd.getParent(), GoFunctionDeclaration.class);
-        }
-
-        final String fnArguments = GoUtil.InspectionGenFuncArgs(e);
-
+    public void invoke(@NotNull final Project project,
+                       @NotNull PsiFile file,
+                       @Nullable("is null when called from inspection") Editor editor,
+                       @NotNull final PsiElement startElement, @NotNull PsiElement endElement) {
+        final PsiElement e;
         final int insertPoint;
-        if (fd != null) {
-            insertPoint = fd.getTextRange().getEndOffset();
+        final Editor wEditor;
+        final PsiFile wFile;
+        final String fnArguments = GoUtil.InspectionGenFuncArgs(startElement);
+        if (workingFile == null) {
+            wFile = file;
+            wEditor = editor;
+            e = startElement;
+            GoFunctionDeclaration fd = findParentOfType(e, GoFunctionDeclaration.class);
+            while (fd instanceof GoLiteralFunction) {
+                fd = findParentOfType(fd.getParent(), GoFunctionDeclaration.class);
+            }
+
+
+            if (fd != null) {
+                insertPoint = fd.getTextRange().getEndOffset();
+            } else {
+                insertPoint = file.getTextRange().getEndOffset();
+            }
+
         } else {
-            insertPoint = file.getTextRange().getEndOffset();
+            e = startElement.getLastChild();
+            insertPoint = workingFile.getTextLength();
+            FileEditorManager.getInstance(project).openFile(workingFile.getVirtualFile(), true, true);
+            wEditor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+            wFile = this.workingFile;
         }
 
         ApplicationManager.getApplication().runWriteAction(new Runnable() {
             @Override
             public void run() {
-                Document doc = PsiDocumentManager.getInstance(e.getProject()).getDocument(file);
-                doc.insertString(insertPoint, String.format("\n\nfunc %s(%s) {\n}\n", e.getText(), fnArguments));
-                if (editor != null) {
-                    int line = doc.getLineNumber(insertPoint);
+                Document doc = PsiDocumentManager.getInstance(project).getDocument(wFile);
+                int insertPoint1 = insertPoint;
+                if (((GoFile) wFile).getPackageName().equals("")) {
+                    String packStr = String.format("\npackage %s", startElement.getFirstChild().getText());
+                    doc.insertString(insertPoint1, packStr);
+                    insertPoint1 += packStr.length();
+                }
+                doc.insertString(insertPoint1, String.format("\n\nfunc %s(%s) {\n}\n", e.getText(), fnArguments));
+                if (wEditor != null) {
+                    int line = doc.getLineNumber(insertPoint1);
                     int offset = doc.getLineEndOffset(line + 2);
-                    editor.getCaretModel().moveToOffset(offset);
-                    reformatLines(file, editor, line, line + 3);
-                    pressEnter(editor);
+                    wEditor.getCaretModel().moveToOffset(offset);
+                    reformatLines(wFile, wEditor, line, line + 3);
+                    pressEnter(wEditor);
+
                 }
             }
         });
