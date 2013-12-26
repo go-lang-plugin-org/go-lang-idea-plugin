7c104eff31b0f0a0ca42bbae9776be9a7f3596d5
Sergey Ignatov <ignatovs@gmail.com>
2013-12-27 00:30:22 +0400

    optimize imports

--- src/com/goide/psi/impl/GoCompositeElementImpl.java
+++ src/com/goide/psi/impl/GoCompositeElementImpl.java
@@ -2,10 +2,7 @@ package com.goide.psi.impl;
 
 import com.goide.psi.GoCompositeElement;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.extapi.psi.PsiElementBase;
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.impl.source.tree.CompositePsiElement;
-import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NotNull;
 
 public class GoCompositeElementImpl extends ASTWrapperPsiElement implements GoCompositeElement {
