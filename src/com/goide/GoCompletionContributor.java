6b6f837f88aa12d8fc8f559ee31bbc99034e9731
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2013-12-27 15:20:48 +0400

    keywords completion

---
+++ src/com/goide/GoCompletionContributor.java
@@ -0,0 +1,58 @@
+package com.goide;
+
+import com.goide.psi.GoFile;
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.lang.parser.GeneratedParserUtilBase;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileFactory;
+import com.intellij.psi.impl.source.tree.TreeUtil;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+import static com.intellij.patterns.PlatformPatterns.instanceOf;
+import static com.intellij.patterns.PlatformPatterns.psiElement;
+
+public class GoCompletionContributor extends CompletionContributor {
+  public GoCompletionContributor() {
+    extend(CompletionType.BASIC, psiElement().inFile(instanceOf(GoFile.class)), new CompletionProvider<CompletionParameters>() {
+      @Override
+      protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result) {
+        PsiElement position = parameters.getPosition();
+        for (String keyword : suggestKeywords(position)) {
+          result.addElement(createKeywordLookupElement(keyword));
+        }
+      }
+    });
+  }
+
+  private static LookupElement createKeywordLookupElement(String keyword) {
+    return LookupElementBuilder.create(keyword).withBoldness(true);
+  }
+
+  private static Collection<String> suggestKeywords(PsiElement position) {
+    TextRange posRange = position.getTextRange();
+    GoFile posFile = (GoFile) position.getContainingFile();
+    final TextRange range = new TextRange(0, posRange.getStartOffset());
+    final String text = range.isEmpty() ? CompletionInitializationContext.DUMMY_IDENTIFIER : range.substring(posFile.getText());
+
+    PsiFile file = PsiFileFactory.getInstance(posFile.getProject()).createFileFromText("a.go", GoLanguage.INSTANCE, text, true, false);
+    int completionOffset = posRange.getStartOffset() - range.getStartOffset();
+    GeneratedParserUtilBase.CompletionState state = new GeneratedParserUtilBase.CompletionState(completionOffset) {
+      @Override
+      public String convertItem(Object o) {
+        if (o instanceof IElementType && GoParserDefinition.KEYWORDS.contains((IElementType) o)) return o.toString();
+        return o instanceof String ? (String) o : null;
+      }
+    };
+    file.putUserData(GeneratedParserUtilBase.COMPLETION_STATE_KEY, state);
+    TreeUtil.ensureParsed(file.getNode());
+    return state.items;
+  }
+}
