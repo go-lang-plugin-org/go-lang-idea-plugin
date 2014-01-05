5e63626a9b198aa118fa742280788de782a05372
Sergey Ignatov <ignatovs@gmail.com>
2014-01-05 23:34:38 +0400

    add description provider

---
+++ src/com/goide/refactor/GoDescriptionProvider.java
@@ -0,0 +1,34 @@
+package com.goide.refactor;
+
+import com.goide.psi.*;
+import com.intellij.psi.ElementDescriptionLocation;
+import com.intellij.psi.ElementDescriptionProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.usageView.UsageViewLongNameLocation;
+import com.intellij.usageView.UsageViewNodeTextLocation;
+import com.intellij.usageView.UsageViewShortNameLocation;
+import com.intellij.usageView.UsageViewTypeLocation;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class GoDescriptionProvider implements ElementDescriptionProvider {
+  @Nullable
+  @Override
+  public String getElementDescription(@NotNull PsiElement o, @NotNull ElementDescriptionLocation location) {
+    if (o instanceof GoNamedElement && location == UsageViewNodeTextLocation.INSTANCE) {
+      return getElementDescription(o, UsageViewTypeLocation.INSTANCE) + " " +
+             "'" + getElementDescription(o, UsageViewShortNameLocation.INSTANCE) + "'";
+    }
+    if (o instanceof GoNamedElement && (location == UsageViewShortNameLocation.INSTANCE || location == UsageViewLongNameLocation.INSTANCE)) {
+      return ((GoNamedElement)o).getName();
+    }
+    if (location == UsageViewTypeLocation.INSTANCE) {
+      if (o instanceof GoMethodDeclaration) return "Method";
+      if (o instanceof GoFunctionDeclaration) return "Function";
+      if (o instanceof GoVarDefinition) return "Variable";
+      if (o instanceof GoParamDefinition) return "Parameter";
+      if (o instanceof GoTypeSpec) return "Type";
+    }
+    return null;
+  }
+}
