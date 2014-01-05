8762fc12cd242c2cf2387655eb385f56b247e3ca
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-01-06 09:32:22 +1100

    Validate run configuration form

--- src/com/goide/runconfig/application/GoApplicationConfiguration.java
+++ src/com/goide/runconfig/application/GoApplicationConfiguration.java
@@ -1,20 +1,26 @@
 package com.goide.runconfig.application;
 
+import com.goide.psi.GoFile;
+import com.goide.psi.impl.GoFunctionDeclarationImpl;
+import com.goide.psi.impl.GoPsiImplUtil;
 import com.goide.runconfig.GoModuleBasedConfiguration;
 import com.goide.runconfig.GoRunConfigurationBase;
 import com.goide.runconfig.GoRunner;
 import com.goide.runconfig.application.ui.GoRunConfigurationEditorForm;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.Executor;
-import com.intellij.execution.configurations.ConfigurationType;
-import com.intellij.execution.configurations.ModuleBasedConfiguration;
-import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.configurations.*;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 
 public class GoApplicationConfiguration extends GoRunConfigurationBase<GoApplicationRunningState> {
   private String myParams = "";
@@ -43,6 +49,26 @@ public class GoApplicationConfiguration extends GoRunConfigurationBase<GoApplica
   @Override
   protected GoApplicationRunningState newRunningState(ExecutionEnvironment env, Module module) {
     return new GoApplicationRunningState(env, module, this);
+  }
+
+  @Override
+  public void checkConfiguration() throws RuntimeConfigurationException {
+    GoModuleBasedConfiguration configurationModule = getConfigurationModule();
+    configurationModule.checkForWarning();
+
+    Module module = configurationModule.getModule();
+    if (module == null) return;
+    VirtualFile file = VfsUtil.findFileByIoFile(new File(myFilePath), false);
+    if (file == null) throw new RuntimeConfigurationError("Main file is not specified");
+    PsiFile psiFile = PsiManager.getInstance(getProject()).findFile(file);
+    if (psiFile == null || !(psiFile instanceof GoFile)) {
+      throw new RuntimeConfigurationError("Main file is invalid");
+    }
+    GoFunctionDeclarationImpl mainFunction = GoPsiImplUtil.findMainFunction((GoFile)psiFile);
+    if (mainFunction == null) {
+      throw new RuntimeConfigurationError("Main file doesn't contain main function");
+    }
+    // todo: check package
   }
 
   @Override
