6009a0a3f26ae1edda379883e09afa8de26221f3
Sergey Ignatov <ignatovs@gmail.com>
2014-01-06 17:58:54 +0400

    add help topic

--- src/com/goide/GoFindUsagesProvider.java
+++ src/com/goide/GoFindUsagesProvider.java
@@ -1,18 +1,33 @@
 package com.goide;
 
 import com.goide.psi.GoNamedElement;
-import com.intellij.lang.findUsages.EmptyFindUsagesProvider;
+import com.intellij.find.impl.HelpID;
+import com.intellij.lang.cacheBuilder.WordsScanner;
+import com.intellij.lang.findUsages.FindUsagesProvider;
 import com.intellij.psi.ElementDescriptionUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.usageView.UsageViewLongNameLocation;
 import com.intellij.usageView.UsageViewNodeTextLocation;
 import com.intellij.usageView.UsageViewTypeLocation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class GoFindUsagesProvider extends EmptyFindUsagesProvider {
+public class GoFindUsagesProvider implements FindUsagesProvider {
+  @Nullable
+  @Override
+  public WordsScanner getWordsScanner() {
+    return null;
+  }
+
   @Override
   public boolean canFindUsagesFor(@NotNull PsiElement psiElement) {
     return psiElement instanceof GoNamedElement;
+  }
+
+  @Nullable
+  @Override
+  public String getHelpId(@NotNull PsiElement psiElement) {
+    return HelpID.FIND_OTHER_USAGES;
   }
 
   @NotNull
