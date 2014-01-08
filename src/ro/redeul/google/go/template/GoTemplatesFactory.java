8b1331107a2ca59fd5fafa44b4f258546aa889c9
Florin Patan <florinpatan@gmail.com>
2014-01-08 21:22:45 +0100

    Improved platform support

--- src/ro/redeul/google/go/template/GoTemplatesFactory.java
+++ src/ro/redeul/google/go/template/GoTemplatesFactory.java
@@ -2,9 +2,9 @@ package ro.redeul.google.go.template;
 
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.module.WebModuleBuilder;
-import com.intellij.openapi.module.WebModuleType;
 import com.intellij.platform.ProjectTemplate;
 import com.intellij.platform.ProjectTemplatesFactory;
+import com.intellij.util.PlatformUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class GoTemplatesFactory extends ProjectTemplatesFactory {
@@ -12,17 +12,17 @@ public class GoTemplatesFactory extends ProjectTemplatesFactory {
   @NotNull
   @Override
   public String[] getGroups() {
+      if (PlatformUtils.isIntelliJ()) {
+          return new String[]{};
+      }
+
       return new String[]{WebModuleBuilder.GROUP_NAME};
   }
 
   @NotNull
   @Override
   public ProjectTemplate[] createTemplates(String group, WizardContext context) {
-
-      // Hack to support IDEA IC and non-IDEA
-      try {
-          WebModuleType module = WebModuleType.getInstance();
-      } catch (Exception ignored) {
+      if (PlatformUtils.isIntelliJ()) {
           return ProjectTemplate.EMPTY_ARRAY;
       }
 
