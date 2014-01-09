695e8c3d88493580f740b6992f9f6db1063df17c
Joe Fitzgerald <jfitzgerald@gopivotal.com>
2014-01-08 17:32:02 -0700

    Ignore Failing Tests Per @dlsniper Who Mentioned @mtoader Is Refactoring The Formatter

--- test/ro/redeul/google/go/formatter/GoFormatterTest.java
+++ test/ro/redeul/google/go/formatter/GoFormatterTest.java
@@ -4,16 +4,23 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import ro.redeul.google.go.GoEditorAwareTestCase;
 import ro.redeul.google.go.lang.psi.GoFile;
+import org.junit.Ignore;
 
 import static ro.redeul.google.go.util.EditorUtil.reformatPositions;
 
 public class GoFormatterTest extends GoEditorAwareTestCase {
     public void testTopLevel() throws Exception { doTest(); }
     public void testBinaryExpressions() throws Exception { doTest(); }
-    public void testAssignment() throws Exception { doTest(); }
+    public void testAssignment() throws Exception { 
+        // TODO: Fix Formatter Test
+        // doTest(); 
+    }
     public void testType() throws Exception { doTest(); }
     public void testCastFunc() throws Exception { doTest(); }
-    public void testFunctionCall() throws Exception { doTest(); }
+    public void testFunctionCall() throws Exception { 
+        // TODO: Fix Formatter Test
+        // doTest(); 
+    }
     public void testInterfaceType() throws Exception { doTest(); }
     public void testCallParameters() throws Exception { doTest(); }
     public void testComments() throws Exception { doTest(); }
