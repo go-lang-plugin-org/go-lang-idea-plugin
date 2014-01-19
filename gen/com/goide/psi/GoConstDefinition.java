b2d2992f5b435750b534c9730054fbfdcdf4e53c
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-01-19 22:08:56 +0400

    Regenerate psi

--- gen/com/goide/psi/GoConstDefinition.java
+++ gen/com/goide/psi/GoConstDefinition.java
@@ -1,11 +1,12 @@
 // This is a generated file. Not intended for manual editing.
 package com.goide.psi;
 
-import java.util.List;
-import org.jetbrains.annotations.*;
+import com.goide.stubs.GoConstDefinitionStub;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.StubBasedPsiElement;
+import org.jetbrains.annotations.NotNull;
 
-public interface GoConstDefinition extends GoNamedElement {
+public interface GoConstDefinition extends GoNamedElement, StubBasedPsiElement<GoConstDefinitionStub> {
 
   @NotNull
   PsiElement getIdentifier();
--- gen/com/goide/psi/impl/GoConstDefinitionImpl.java
+++ gen/com/goide/psi/impl/GoConstDefinitionImpl.java
@@ -1,23 +1,24 @@
 // This is a generated file. Not intended for manual editing.
 package com.goide.psi.impl;
 
-import java.util.List;
-import org.jetbrains.annotations.*;
+import com.goide.psi.GoConstDefinition;
+import com.goide.psi.GoVisitor;
+import com.goide.stubs.GoConstDefinitionStub;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.util.PsiTreeUtil;
-import static com.goide.GoTypes.*;
-import com.goide.psi.*;
 import com.intellij.psi.stubs.IStubElementType;
+import org.jetbrains.annotations.NotNull;
 
-public class GoConstDefinitionImpl extends GoNamedElementImpl<?> implements GoConstDefinition {
+import static com.goide.GoTypes.IDENTIFIER;
+
+public class GoConstDefinitionImpl extends GoNamedElementImpl<GoConstDefinitionStub> implements GoConstDefinition {
 
   public GoConstDefinitionImpl(ASTNode node) {
     super(node);
   }
 
-  public GoConstDefinitionImpl(? stub, IStubElementType nodeType) {
+  public GoConstDefinitionImpl(GoConstDefinitionStub stub, IStubElementType nodeType) {
     super(stub, nodeType);
   }
 
