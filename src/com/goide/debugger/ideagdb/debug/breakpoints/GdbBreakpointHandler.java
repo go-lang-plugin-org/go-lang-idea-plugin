7b71cca313d0a13ecbd4e607102c869c778641dd
Sergey Ignatov <ignatovs@gmail.com>
2014-01-25 14:01:18 +0400

    rename and small cleanup

--- src/com/goide/debugger/ideagdb/debug/breakpoints/GdbBreakpointHandler.java
+++ src/com/goide/debugger/ideagdb/debug/breakpoints/GdbBreakpointHandler.java
@@ -15,22 +15,19 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class GdbBreakpointHandler extends
-                                  XBreakpointHandler<XLineBreakpoint<GdbBreakpointProperties>> {
-  private static final Logger m_log =
-    Logger.getInstance("#com.goide.debugger.ideagdb.debug.breakpoints.GdbBreakpointHandler");
+public class GdbBreakpointHandler extends XBreakpointHandler<XLineBreakpoint<GdbBreakpointProperties>> {
+  private static final Logger LOG = Logger.getInstance(GdbBreakpointHandler.class);
 
-  private Gdb m_gdb;
-  private GdbDebugProcess m_debugProcess;
+  private final Gdb myGdb;
+  private final GdbDebugProcess myDebugProcess;
 
   // The breakpoints that have been set and their GDB breakpoint numbers
-  private final BidirectionalMap<Integer, XLineBreakpoint<GdbBreakpointProperties>>
-    m_breakpoints = new BidirectionalMap<Integer, XLineBreakpoint<GdbBreakpointProperties>>();
+  private final BidirectionalMap<Integer, XLineBreakpoint<GdbBreakpointProperties>> myBreakpoints = new BidirectionalMap<Integer, XLineBreakpoint<GdbBreakpointProperties>>();
 
   public GdbBreakpointHandler(Gdb gdb, GdbDebugProcess debugProcess) {
     super(GdbBreakpointType.class);
-    m_gdb = gdb;
-    m_debugProcess = debugProcess;
+    myGdb = gdb;
+    myDebugProcess = debugProcess;
   }
 
   /**
@@ -47,14 +44,14 @@ public class GdbBreakpointHandler extends
     Integer number = findBreakpointNumber(breakpoint);
     if (number != null) {
       // Re-enable the breakpoint
-      m_gdb.sendCommand("-break-enable " + number);
+      myGdb.sendCommand("-break-enable " + number);
     }
     else {
       // Set the breakpoint
       XSourcePosition sourcePosition = breakpoint.getSourcePosition();
       String command = "-break-insert -f " + sourcePosition.getFile().getPath() + ":" +
                        (sourcePosition.getLine() + 1);
-      m_gdb.sendCommand(command, new Gdb.GdbEventCallback() {
+      myGdb.sendCommand(command, new Gdb.GdbEventCallback() {
         @Override
         public void onGdbCommandCompleted(GdbEvent event) {
           onGdbBreakpointReady(event, breakpoint);
@@ -74,20 +71,20 @@ public class GdbBreakpointHandler extends
                                    boolean temporary) {
     Integer number = findBreakpointNumber(breakpoint);
     if (number == null) {
-      m_log.error("Cannot remove breakpoint; could not find it in breakpoint table");
+      LOG.error("Cannot remove breakpoint; could not find it in breakpoint table");
       return;
     }
 
     if (!temporary) {
       // Delete the breakpoint
-      m_gdb.sendCommand("-break-delete " + number);
-      synchronized (m_breakpoints) {
-        m_breakpoints.remove(number);
+      myGdb.sendCommand("-break-delete " + number);
+      synchronized (myBreakpoints) {
+        myBreakpoints.remove(number);
       }
     }
     else {
       // Disable the breakpoint
-      m_gdb.sendCommand("-break-disable " + number);
+      myGdb.sendCommand("-break-disable " + number);
     }
   }
 
@@ -98,8 +95,8 @@ public class GdbBreakpointHandler extends
    * @return The breakpoint, or null if it could not be found.
    */
   public XLineBreakpoint<GdbBreakpointProperties> findBreakpoint(int number) {
-    synchronized (m_breakpoints) {
-      return m_breakpoints.get(number);
+    synchronized (myBreakpoints) {
+      return myBreakpoints.get(number);
     }
   }
 
@@ -111,15 +108,15 @@ public class GdbBreakpointHandler extends
    */
   public Integer findBreakpointNumber(XLineBreakpoint<GdbBreakpointProperties> breakpoint) {
     List<Integer> numbers;
-    synchronized (m_breakpoints) {
-      numbers = m_breakpoints.getKeysByValue(breakpoint);
+    synchronized (myBreakpoints) {
+      numbers = myBreakpoints.getKeysByValue(breakpoint);
     }
 
     if (numbers == null || numbers.isEmpty()) {
       return null;
     }
     else if (numbers.size() > 1) {
-      m_log.warn("Found multiple breakpoint numbers for breakpoint; only returning the " +
+      LOG.warn("Found multiple breakpoint numbers for breakpoint; only returning the " +
                  "first");
     }
     return numbers.get(0);
@@ -134,35 +131,35 @@ public class GdbBreakpointHandler extends
   private void onGdbBreakpointReady(GdbEvent event,
                                     XLineBreakpoint<GdbBreakpointProperties> breakpoint) {
     if (event instanceof GdbErrorEvent) {
-      m_debugProcess.getSession().updateBreakpointPresentation(breakpoint,
+      myDebugProcess.getSession().updateBreakpointPresentation(breakpoint,
                                                                AllIcons.Debugger.Db_invalid_breakpoint, ((GdbErrorEvent)event).message);
       return;
     }
     if (!(event instanceof GdbBreakpoint)) {
-      m_debugProcess.getSession().updateBreakpointPresentation(breakpoint,
+      myDebugProcess.getSession().updateBreakpointPresentation(breakpoint,
                                                                AllIcons.Debugger.Db_invalid_breakpoint,
                                                                "Unexpected data received from GDB");
-      m_log.warn("Unexpected event " + event + " received from -break-insert request");
+      LOG.warn("Unexpected event " + event + " received from -break-insert request");
       return;
     }
 
     // Save the breakpoint
     GdbBreakpoint gdbBreakpoint = (GdbBreakpoint)event;
     if (gdbBreakpoint.number == null) {
-      m_debugProcess.getSession().updateBreakpointPresentation(breakpoint,
+      myDebugProcess.getSession().updateBreakpointPresentation(breakpoint,
                                                                AllIcons.Debugger.Db_invalid_breakpoint,
                                                                "No breakpoint number received from GDB");
-      m_log.warn("No breakpoint number received from GDB after -break-insert request");
+      LOG.warn("No breakpoint number received from GDB after -break-insert request");
       return;
     }
 
-    synchronized (m_breakpoints) {
-      m_breakpoints.put(gdbBreakpoint.number, breakpoint);
+    synchronized (myBreakpoints) {
+      myBreakpoints.put(gdbBreakpoint.number, breakpoint);
     }
 
     // Mark the breakpoint as set
     // TODO: Don't do this yet if the breakpoint is pending
-    m_debugProcess.getSession().updateBreakpointPresentation(breakpoint,
+    myDebugProcess.getSession().updateBreakpointPresentation(breakpoint,
                                                              AllIcons.Debugger.Db_verified_breakpoint, null);
   }
 }
