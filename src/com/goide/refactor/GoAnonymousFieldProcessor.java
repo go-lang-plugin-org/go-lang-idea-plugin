f90622307a29ea07cc721abe112e7addcd4803ca
Sergey Ignatov <ignatovs@gmail.com>
2014-02-05 22:58:41 +0400

    rename anonymous field, test tdb, i'm too tired now

---
+++ src/com/goide/refactor/GoAnonymousFieldProcessor.java
@@ -0,0 +1,43 @@
+package com.goide.refactor;
+
+import com.goide.psi.GoAnonymousFieldDefinition;
+import com.goide.psi.GoTypeSpec;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.SearchScope;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.refactoring.rename.RenamePsiElementProcessor;
+import com.intellij.util.Query;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public class GoAnonymousFieldProcessor extends RenamePsiElementProcessor {
+  @Override
+  public boolean canProcessElement(@NotNull PsiElement element) {
+    return
+      element instanceof GoTypeSpec ||
+      element instanceof GoAnonymousFieldDefinition;
+  }
+
+  @Override
+  public void prepareRenaming(PsiElement element, String newName, Map<PsiElement, String> allRenames, SearchScope scope) {
+    if (element instanceof GoTypeSpec) {
+      Query<PsiReference> search = ReferencesSearch.search(element, scope);
+      for (PsiReference ref : search) {
+        PsiElement refElement = ref.getElement();
+        PsiElement parent = refElement == null ? null : refElement.getParent();
+        if (parent instanceof GoAnonymousFieldDefinition) {
+          allRenames.put(parent, newName);
+        }
+      }
+    }
+    else if (element instanceof GoAnonymousFieldDefinition) {
+      PsiReference reference = ((GoAnonymousFieldDefinition)element).getTypeReferenceExpression().getReference();
+      PsiElement type = reference != null ? reference.resolve() : null;
+      if (type instanceof GoTypeSpec) {
+        allRenames.put(type, newName);
+      }
+    }
+  }
+}
