83c6d3420be5828e1876a2cf95903ed749db9bb9
Sergey Ignatov <ignatovs@gmail.com>
2014-02-12 17:04:06 +0400

    better documentation for types and etc.

--- src/com/goide/GoDocumentationProvider.java
+++ src/com/goide/GoDocumentationProvider.java
@@ -1,11 +1,13 @@
 package com.goide;
 
 import com.goide.psi.GoNamedElement;
+import com.goide.psi.GoTopLevelDeclaration;
 import com.intellij.lang.documentation.AbstractDocumentationProvider;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiWhiteSpace;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Function;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
@@ -17,7 +19,10 @@ public class GoDocumentationProvider extends AbstractDocumentationProvider {
   @Override
   public String generateDoc(PsiElement element, PsiElement originalElement) {
     if (element instanceof GoNamedElement) {
-      List<PsiComment> comments = getPreviousNonWsComment(element);
+      GoTopLevelDeclaration topLevel = PsiTreeUtil.getParentOfType(element, GoTopLevelDeclaration.class);
+      PsiElement[] children = PsiTreeUtil.getChildrenOfType(topLevel, element.getClass());
+      boolean alone = children != null && children.length == 1 && children[0].equals(element);
+      List<PsiComment> comments = getPreviousNonWsComment(alone ? topLevel : element);
       if (!comments.isEmpty()) return getCommentText(comments);
     }
     return null;
