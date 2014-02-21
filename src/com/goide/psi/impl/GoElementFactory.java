89ef749c977d68f64a6119e7c72ac8a0b831f390
Sergey Ignatov <ignatovs@gmail.com>
2014-02-22 01:53:43 +0400

    cleanup

--- src/com/goide/psi/impl/GoElementFactory.java
+++ src/com/goide/psi/impl/GoElementFactory.java
@@ -3,8 +3,6 @@ package com.goide.psi.impl;
 import com.goide.GoLanguage;
 import com.goide.psi.GoFile;
 import com.goide.psi.GoImportString;
-import com.goide.psi.GoReferenceExpression;
-import com.goide.psi.GoTypeReferenceExpression;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFileFactory;
@@ -17,20 +15,8 @@ public class GoElementFactory {
   }
 
   @NotNull
-  public static GoReferenceExpression createReferenceFromText(@NotNull Project project, @NotNull String text) {
-    GoFile file = createFileFromText(project, "package f; func f() { " + text + "}");
-    return PsiTreeUtil.findChildOfType(file.getFunctions().get(0).getBlock(), GoReferenceExpression.class);
-  }
-
-  @NotNull
   private static GoFile createFileFromText(@NotNull Project project, @NotNull String text) {
     return (GoFile)PsiFileFactory.getInstance(project).createFileFromText("a.go", GoLanguage.INSTANCE, text);
-  }
-
-  @NotNull
-  public static GoTypeReferenceExpression createTypeReferenceFromText(@NotNull Project project, String text) {
-    GoFile file = createFileFromText(project, "package f; var oo " + text + ";");
-    return PsiTreeUtil.findChildOfType(file, GoTypeReferenceExpression.class);
   }
 
   @NotNull
