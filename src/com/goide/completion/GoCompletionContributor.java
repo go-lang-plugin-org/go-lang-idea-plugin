b2b37bb3fff3b7f1def5d38740a6620a7641d1f3
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-02-25 00:29:54 +0400

    Initial keyword completion

--- src/com/goide/completion/GoCompletionContributor.java
+++ src/com/goide/completion/GoCompletionContributor.java
@@ -1,40 +1,36 @@
 package com.goide.completion;
 
-import com.goide.GoLanguage;
-import com.goide.GoParserDefinition;
-import com.goide.GoTypes;
 import com.goide.psi.*;
 import com.goide.psi.impl.GoPsiImplUtil;
 import com.goide.stubs.index.GoFunctionIndex;
 import com.goide.util.GoUtil;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.completion.util.ParenthesesInsertHandler;
+import com.intellij.codeInsight.lookup.AutoCompletionPolicy;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.codeInsight.template.Template;
 import com.intellij.codeInsight.template.TemplateManager;
 import com.intellij.codeInsight.template.impl.TemplateManagerImpl;
 import com.intellij.codeInsight.template.impl.TemplateSettings;
-import com.intellij.lang.ASTNode;
-import com.intellij.lang.parser.GeneratedParserUtilBase;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.patterns.PsiElementPattern;
-import com.intellij.psi.*;
-import com.intellij.psi.formatter.FormatterUtil;
-import com.intellij.psi.impl.source.tree.TreeUtil;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiErrorElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.stubs.StubIndex;
-import com.intellij.psi.tree.IElementType;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 
-import static com.intellij.patterns.PlatformPatterns.instanceOf;
 import static com.intellij.patterns.PlatformPatterns.psiElement;
 
 public class GoCompletionContributor extends CompletionContributor {
@@ -47,64 +43,66 @@ public class GoCompletionContributor extends CompletionContributor {
   public static final int PACKAGE_PRIORITY = 5;
 
   public GoCompletionContributor() {
-    extend(CompletionType.BASIC, inGoFile(), new CompletionProvider<CompletionParameters>() {
-      @Override
-      protected void addCompletions(@NotNull CompletionParameters parameters,
-                                    ProcessingContext context,
-                                    @NotNull final CompletionResultSet result) {
-        PsiElement position = parameters.getPosition();
-        ASTNode prev = FormatterUtil.getPreviousNonWhitespaceSibling(position.getNode());
-        if (prev != null && prev.getElementType() == GoTypes.DOT) return;
-        if (position.getNode().getElementType() == GoTypes.STRING) return;
-        PsiElement parent = position.getParent();
-        if (parent.getParent() instanceof GoSelectorExpr) return;
-        if (position instanceof PsiComment) return;
-        for (String keyword : suggestKeywords(position)) {
-          result.addElement(createKeywordLookupElement(keyword));
-        }
-        
-      }
-    });
+    extend(CompletionType.BASIC, packagePattern(), new KeywordCompletionProvider(AutoCompletionPolicy.ALWAYS_AUTOCOMPLETE, "package"));
+    extend(CompletionType.BASIC, importPattern(), new KeywordCompletionProvider("import"));
+    extend(CompletionType.BASIC, topLevelPattern(), new KeywordCompletionProvider("const", "var", "func", "type", "interface"));
   }
-
+  
   private static PsiElementPattern.Capture<PsiElement> inGoFile() {
-    return psiElement().inFile(instanceOf(GoFile.class));
+    return psiElement().inFile(psiElement(GoFile.class));
   }
 
-  @NotNull
-  private static LookupElement createKeywordLookupElement(@NotNull final String keyword) {
-    return LookupElementBuilder.create(keyword).withBoldness(true).withInsertHandler(new InsertHandler<LookupElement>() {
-      @Override
-      public void handleInsert(InsertionContext context, LookupElement item) {
-        TemplateManagerImpl templateManager = (TemplateManagerImpl)TemplateManager.getInstance(context.getProject());
-        Template template = TemplateSettings.getInstance().getTemplateById("go_lang_" + keyword);
-        if (template != null) {
-          context.getEditor().getDocument().deleteString(context.getStartOffset(), context.getTailOffset());
-          templateManager.startTemplate(context.getEditor(), template);
-        }
-      }
-    });
+  private static PsiElementPattern.Capture<PsiElement> topLevelPattern() {
+    return psiElement().withParent(psiElement(PsiErrorElement.class).withParent(GoFile.class));
   }
 
-  @NotNull
-  private static Collection<String> suggestKeywords(@NotNull PsiElement position) {
-    TextRange posRange = position.getTextRange();
-    GoFile posFile = (GoFile)position.getContainingFile();
-    TextRange range = new TextRange(0, posRange.getStartOffset());
-    String text = range.isEmpty() ? CompletionInitializationContext.DUMMY_IDENTIFIER : range.substring(posFile.getText());
 
-    PsiFile file = PsiFileFactory.getInstance(posFile.getProject()).createFileFromText("a.go", GoLanguage.INSTANCE, text, true, false);
-    int completionOffset = posRange.getStartOffset() - range.getStartOffset();
-    GeneratedParserUtilBase.CompletionState state = new GeneratedParserUtilBase.CompletionState(completionOffset) {
-      @Override
-      public String convertItem(Object o) {
-        if (o instanceof IElementType && GoParserDefinition.KEYWORDS.contains((IElementType)o)) return o.toString();
-        return o instanceof String ? (String)o : null;
+  private static PsiElementPattern.Capture<PsiElement> importPattern() {
+    return psiElement().withParent(psiElement(PsiErrorElement.class).afterSiblingSkipping(psiElement().whitespace(),
+                                                                                          psiElement(GoImportList.class)));
+  }
+
+  private static PsiElementPattern.Capture<PsiElement> packagePattern() {
+    return psiElement().withParent(psiElement(PsiErrorElement.class).withParent(GoFile.class).isFirstAcceptedChild(psiElement()));
+  }
+
+  private static class KeywordCompletionProvider extends CompletionProvider<CompletionParameters> {
+    @Nullable private final AutoCompletionPolicy myCompletionPolicy;
+    private final String[] myKeywords;
+
+    private KeywordCompletionProvider(String... keywords) {
+      this(null, keywords);
+    }
+
+    private KeywordCompletionProvider(@Nullable AutoCompletionPolicy completionPolicy, String... keywords) {
+      myCompletionPolicy = completionPolicy;
+      myKeywords = keywords;
+    }
+
+    @Override
+    protected void addCompletions(@NotNull CompletionParameters parameters,
+                                  ProcessingContext context,
+                                  @NotNull CompletionResultSet result) {
+      for (String keyword : myKeywords) {
+        result.addElement(createKeywordLookupElement(keyword));
       }
-    };
-    file.putUserData(GeneratedParserUtilBase.COMPLETION_STATE_KEY, state);
-    TreeUtil.ensureParsed(file.getNode());
-    return state.items;
+    }
+
+    private LookupElement createKeywordLookupElement(final String keyword) {
+      LookupElementBuilder result =
+        LookupElementBuilder.create(keyword).withBoldness(true).withInsertHandler(new InsertHandler<LookupElement>() {
+          @Override
+          public void handleInsert(InsertionContext context, LookupElement item) {
+            TemplateManagerImpl templateManager = (TemplateManagerImpl)TemplateManager.getInstance(context.getProject());
+            Template template = TemplateSettings.getInstance().getTemplateById("go_lang_" + keyword);
+            if (template != null) {
+              context.getEditor().getDocument().deleteString(context.getStartOffset(), context.getTailOffset());
+              templateManager.startTemplate(context.getEditor(), template);
+            }
+          }
+        });
+      return myCompletionPolicy != null ? myCompletionPolicy.applyPolicy(result) : result;
+    }
   }
   
   public static class AutoImport extends CompletionContributor {
