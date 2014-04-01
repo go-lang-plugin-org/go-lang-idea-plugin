2353f6885f2b2e8f8214f35a5697fce4c9292120
Sergey Ignatov <ignatovs@gmail.com>
2014-04-02 02:07:06 +0400

    reorder methods

--- tests/com/goide/GoCodeInsightFixtureTestCase.java
+++ tests/com/goide/GoCodeInsightFixtureTestCase.java
@@ -1,10 +1,52 @@
 package com.goide;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.projectRoots.ProjectJdkTable;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.SdkModificator;
+import com.intellij.openapi.projectRoots.impl.ProjectJdkImpl;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import org.jetbrains.annotations.NotNull;
 
 abstract public class GoCodeInsightFixtureTestCase extends LightPlatformCodeInsightFixtureTestCase {
   @Override
   protected String getTestDataPath() {
     return "testData/" + getBasePath();
+  }
+
+  protected static DefaultLightProjectDescriptor createMockProjectDescriptor() {
+    return new DefaultLightProjectDescriptor() {
+      @Override
+      public Sdk getSdk() {
+        String version = "1.1.2";
+        return createMockSdk("testData/mockSdk-" + version + "/", version);
+      }
+    };
+  }
+
+  protected void setUpProjectSdk() {
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      @Override
+      public void run() {
+        Sdk sdk = getProjectDescriptor().getSdk();
+        ProjectJdkTable.getInstance().addJdk(sdk);
+        ProjectRootManager.getInstance(myFixture.getProject()).setProjectSdk(sdk);
+      }
+    });
+  }
+
+  @NotNull
+  private static Sdk createMockSdk(@NotNull String sdkHome, @NotNull String version) {
+    GoSdkType instance = GoSdkType.getInstance();
+    String release = "Go " + version;
+    Sdk sdk = new ProjectJdkImpl(release, instance);
+    SdkModificator sdkModificator = sdk.getSdkModificator();
+    sdkModificator.setHomePath(sdkHome);
+    sdkModificator.setVersionString(release); // must be set after home path, otherwise setting home path clears the version string
+    sdkModificator.commitChanges();
+    instance.setupSdkPaths(sdk);
+    return sdk;
   }
 }
--- tests/com/goide/inspections/GoHighlightingTest.java
+++ tests/com/goide/inspections/GoHighlightingTest.java
@@ -1,20 +1,11 @@
 package com.goide.inspections;
 
 import com.goide.GoCodeInsightFixtureTestCase;
-import com.goide.GoSdkType;
 import com.goide.inspections.unresolved.GoAssignmentToConstantInspection;
 import com.goide.inspections.unresolved.GoUnresolvedReferenceInspection;
 import com.goide.inspections.unresolved.GoUnusedFunctionInspection;
 import com.goide.inspections.unresolved.GoUnusedVariableInspection;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.projectRoots.ProjectJdkTable;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.projectRoots.SdkModificator;
-import com.intellij.openapi.projectRoots.impl.ProjectJdkImpl;
-import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.testFramework.LightProjectDescriptor;
-import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
-import org.jetbrains.annotations.NotNull;
 
 public class GoHighlightingTest extends GoCodeInsightFixtureTestCase {
   @Override
@@ -44,43 +35,13 @@ public class GoHighlightingTest extends GoCodeInsightFixtureTestCase {
     return false;
   }
 
-  protected void setUpProjectSdk() {
-    ApplicationManager.getApplication().runWriteAction(new Runnable() {
-      @Override
-      public void run() {
-        Sdk sdk = getProjectDescriptor().getSdk();
-        ProjectJdkTable.getInstance().addJdk(sdk);
-        ProjectRootManager.getInstance(myFixture.getProject()).setProjectSdk(sdk);
-      }
-    });
-  }
-
-  @Override
-  protected LightProjectDescriptor getProjectDescriptor() {
-    return new DefaultLightProjectDescriptor() {
-      @Override
-      public Sdk getSdk() {
-        String version = "1.1.2";
-        return createMockSdk("testData/mockSdk-" + version + "/", version);
-      }
-    };
-  }
-
-  @NotNull
-  public static Sdk createMockSdk(@NotNull String sdkHome, @NotNull String version) {
-    GoSdkType instance = GoSdkType.getInstance();
-    String release = "Go " + version;
-    Sdk sdk = new ProjectJdkImpl(release, instance);
-    SdkModificator sdkModificator = sdk.getSdkModificator();
-    sdkModificator.setHomePath(sdkHome);
-    sdkModificator.setVersionString(release); // must be set after home path, otherwise setting home path clears the version string
-    sdkModificator.commitChanges();
-    instance.setupSdkPaths(sdk);
-    return sdk;
-  }
-
   public void testSimple() throws Exception { doTest(); }
   public void testStruct() throws Exception { doTest(); }
   public void testBoxes()  throws Exception { doTest(); }
   public void testRanges() throws Exception { doTest(); }
+  
+  @Override
+  protected LightProjectDescriptor getProjectDescriptor() {
+    return createMockProjectDescriptor();
+  }
 }
