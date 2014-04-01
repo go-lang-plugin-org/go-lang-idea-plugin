7c06217407a4b044f1aba57f4cac7660786e43e0
Sergey Ignatov <ignatovs@gmail.com>
2014-04-02 02:12:41 +0400

    add sdk aware completion test

---
+++ tests/com/goide/completion/GoCompletionSdkAwareTest.java
@@ -0,0 +1,20 @@
+package com.goide.completion;
+
+import com.intellij.testFramework.LightProjectDescriptor;
+
+public class GoCompletionSdkAwareTest extends GoCompletionTestBase {
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    setUpProjectSdk();
+  }
+
+  @Override
+  protected LightProjectDescriptor getProjectDescriptor() {
+    return createMockProjectDescriptor();
+  }
+
+  public void testFormatter() throws Exception {
+    doTestInclude("package main; import . \"fmt\"; type alias <caret>", "Formatter");
+  }
+}
