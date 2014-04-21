5aa95f33aa8cd4c7daf9ca43e075e0f72d375cd4
Sergey Ignatov <ignatovs@gmail.com>
2014-04-21 16:19:18 +0400

    better check

--- src/com/goide/util/GoUtil.java
+++ src/com/goide/util/GoUtil.java
@@ -8,15 +8,18 @@ import com.intellij.util.text.CharSequenceHashingStrategy;
 import gnu.trove.THashSet;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GoUtil {
   public static final Pattern FULL = Pattern.compile("\\w+_(\\w+)_(\\w+)");
   public static final Pattern SHORT = Pattern.compile("\\w+_(\\w+)");
-  public static final THashSet<CharSequence> LINUX = ContainerUtil.newTroveSet(CharSequenceHashingStrategy.CASE_INSENSITIVE, "linux", "no", "unix", "posix", "notwin");
-  public static final THashSet<CharSequence> MAC = ContainerUtil.newTroveSet(CharSequenceHashingStrategy.CASE_INSENSITIVE, "darwin", "no", "unix", "posix", "notwin");
-  public static final THashSet<CharSequence> WINDOWS = ContainerUtil.newTroveSet(CharSequenceHashingStrategy.CASE_INSENSITIVE, "windows", "no");
+  public static final Set<CharSequence> LINUX = set("linux", "no", "unix", "posix", "notwin");
+  public static final Set<CharSequence> MAC = set("darwin", "no", "unix", "posix", "notwin");
+  public static final Set<CharSequence> WINDOWS = set("windows", "no");
+  public static final Set<CharSequence> OS = set("openbsd", "plan9", "unix", "linux", "netbsd", "darwin", "dragonfly", "bsd", "windows", 
+                                                 "posix", "freebsd", "notwin");
 
   @NotNull
   public static String replaceLast(@NotNull String src, @NotNull String from) {
@@ -34,19 +37,26 @@ public class GoUtil {
     if (matcher.matches()) {
       String os = matcher.group(1);
       String arch = matcher.group(2);
+      if (!OS.contains(os)) return true;
       return os(os) && (SystemInfo.is64Bit && arch.contains("64") || SystemInfo.is32Bit && arch.contains("386"));
     }
     matcher = SHORT.matcher(name);
     if (matcher.matches()) {
       String os = matcher.group(1);
+      if (!OS.contains(os)) return true;
       return os(os);
     }
     return true;
   }
 
   private static boolean os(@NotNull String os) {
-    return SystemInfo.isLinux ? LINUX.contains(os) : 
+    return SystemInfo.isLinux ? LINUX.contains(os) :
            SystemInfo.isMac ? MAC.contains(os) :
            !SystemInfo.isWindows || WINDOWS.contains(os);
+  }
+
+  @NotNull
+  private static THashSet<CharSequence> set(@NotNull String... strings) {
+    return ContainerUtil.newTroveSet(CharSequenceHashingStrategy.CASE_INSENSITIVE, strings);
   }
 }
