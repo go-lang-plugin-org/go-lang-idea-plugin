32b40cf5f0b44bc37797ff0feead66a8fde08908
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2014-05-31 20:17:04 +0400

    inline

--- src/com/goide/runconfig/application/GoApplicationRunConfigurationType.java
+++ src/com/goide/runconfig/application/GoApplicationRunConfigurationType.java
@@ -2,30 +2,22 @@ package com.goide.runconfig.application;
 
 import com.goide.GoIcons;
 import com.intellij.execution.configurations.ConfigurationFactory;
-import com.intellij.execution.configurations.ConfigurationType;
 import com.intellij.execution.configurations.ConfigurationTypeBase;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.project.Project;
 
 public class GoApplicationRunConfigurationType extends ConfigurationTypeBase {
-
   public GoApplicationRunConfigurationType() {
     super("GoApplicationRunConfiguration", "Go Application", "Go application run configuration", GoIcons.APPLICATION_RUN);
-    addFactory(new GoFactory(this));
+    addFactory(new ConfigurationFactory(this) {
+      public RunConfiguration createTemplateConfiguration(Project project) {
+        return new GoApplicationConfiguration(project, "Go", getInstance());
+      }
+    });
   }
 
   public static GoApplicationRunConfigurationType getInstance() {
     return Extensions.findExtension(CONFIGURATION_TYPE_EP, GoApplicationRunConfigurationType.class);
-  }
-
-  private static class GoFactory extends ConfigurationFactory {
-    public GoFactory(ConfigurationType type) {
-      super(type);
-    }
-
-    public RunConfiguration createTemplateConfiguration(Project project) {
-      return new GoApplicationConfiguration(project, "Go", getInstance());
-    }
   }
 }
