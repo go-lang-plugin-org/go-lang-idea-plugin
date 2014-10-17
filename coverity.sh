35016684272eb9934630c5ad224c286c7d646d84
Florin Patan <florinpatan@gmail.com>
2014-10-18 00:34:29 +0200

    Add TravisCI release support and Coverity integration

--- coverity.sh
+++ coverity.sh
@@ -4,16 +4,17 @@
 
 # Run the tests
 if [ "$1" = "-d" ]; then
-    ant -d -f build-test.xml -DIDEA_HOME=./idea-IC
+    ant -d -f build-package.xml -DIDEA_HOME=./idea-IC
 else
-    ant -f build-test.xml -DIDEA_HOME=./idea-IC
+    ant -f build-package.xml -DIDEA_HOME=./idea-IC
 fi
 
 # Was our build successful?
 stat=$?
 
+# Cleanup
 if [ "${TRAVIS}" != true ]; then
-    ant -f build-test.xml -q clean
+    ant -f build-package.xml -q clean
     rm -rf idea-IC
 fi
 
