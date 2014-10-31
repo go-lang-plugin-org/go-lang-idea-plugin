b6d4c3797ddf62accba51c7d3ace1c5d382ecbdf
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2014-10-31 17:40:21 +0300

    travis integration, v2

--- fetchIdea.sh
+++ fetchIdea.sh
@@ -8,7 +8,7 @@ if [ ! -d ./idea-IC ]; then
     then
         cp ~/Tools/ideaIC-${ideaVersion}.tar.gz .
     else
-        # wget http://download-cf.jetbrains.com/idea/ideaIC-${ideaVersion}.tar.gz
+        wget http://download-cf.jetbrains.com/idea/ideaIC-${ideaVersion}.tar.gz
         # wget http://download.labs.intellij.net/idea/ideaIC-${ideaVersion}.tar.gz
     fi
 
