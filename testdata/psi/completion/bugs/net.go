ab81a194dfa22b33c6833b73518b1257d2c583a7
bronze1man <bronze1man@gmail.com>
2014-11-19 01:32:26 +0800

    add test case for #1006

--- testdata/psi/completion/bugs/net.go
+++ testdata/psi/completion/bugs/net.go
@@ -1,2 +1,25 @@
 package net
 
+type AddrError struct {
+	Err  string
+	Addr string
+}
+
+func (e *AddrError) Error() string {
+	if e == nil {
+		return "<nil>"
+	}
+	s := e.Err
+	if e.Addr != "" {
+		s += " " + e.Addr
+	}
+	return s
+}
+
+func (e *AddrError) Temporary() bool {
+	return false
+}
+
+func (e *AddrError) Timeout() bool {
+	return false
+}
