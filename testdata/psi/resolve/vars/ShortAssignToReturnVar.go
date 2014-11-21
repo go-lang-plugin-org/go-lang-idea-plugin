8e6b1df62a7db6d0f72b8fe245f3d0c94e82dde3
Mihai Toader <mtoader@gmail.com>
2014-11-21 12:33:34 -0800

    Fixed the short var resolution to work

--- testdata/psi/resolve/vars/ShortAssignToReturnVar.go
+++ testdata/psi/resolve/vars/ShortAssignToReturnVar.go
@@ -1,12 +1,7 @@
 package main
 
 func Test(x int) (/*def*/r int) {
-	/*ref*/r = x
-	return
-}
-
-func Test2(x int) (/*def*/a int) {
-	/*ref*/a, /*def*/b := x, 1
-	_ = /*ref*/b
+	/*ref*/r, a := x, 1
+	_ = a
 	return
 }
