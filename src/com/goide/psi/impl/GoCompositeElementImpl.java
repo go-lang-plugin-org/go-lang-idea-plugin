9b03b754d12a6f2e72ccf8462fcf6a1acf1bd61d
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2014-11-29 17:08:29 +0300

    nullable

--- src/com/goide/psi/impl/GoCompositeElementImpl.java
+++ src/com/goide/psi/impl/GoCompositeElementImpl.java
@@ -23,6 +23,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.ResolveState;
 import com.intellij.psi.scope.PsiScopeProcessor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class GoCompositeElementImpl extends ASTWrapperPsiElement implements GoCompositeElement {
   public GoCompositeElementImpl(@NotNull ASTNode node) {
@@ -37,16 +38,16 @@ public class GoCompositeElementImpl extends ASTWrapperPsiElement implements GoCo
   @Override
   public boolean processDeclarations(@NotNull PsiScopeProcessor processor,
                                      @NotNull ResolveState state,
-                                     PsiElement lastParent,
+                                     @Nullable PsiElement lastParent,
                                      @NotNull PsiElement place) {
     return precessDeclarationDefault(this, processor, state, lastParent, place);
   }
 
   public static boolean precessDeclarationDefault(@NotNull GoCompositeElement o,
-                                                  PsiScopeProcessor processor,
-                                                  ResolveState state,
-                                                  PsiElement lastParent,
-                                                  PsiElement place) {
+                                                  @NotNull PsiScopeProcessor processor,
+                                                  @NotNull ResolveState state,
+                                                  @Nullable PsiElement lastParent,
+                                                  @NotNull PsiElement place) {
     return processor.execute(o, state) && ResolveUtil.processChildren(o, processor, state, lastParent, place);
   }
 }
