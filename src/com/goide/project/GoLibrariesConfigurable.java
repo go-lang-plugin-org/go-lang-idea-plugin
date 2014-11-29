5519ed9040ab5344f21f1c653473531e619393c4
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-11-29 17:38:06 +0300

    Add empty configurable for libraries

---
+++ src/com/goide/project/GoLibrariesConfigurable.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2013-2014 Sergey Ignatov, Alexander Zolotov, Mihai Toader
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.goide.project;
+
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class GoLibrariesConfigurable implements SearchableConfigurable {
+  private final Module myModule;
+
+  public GoLibrariesConfigurable(Module module) {
+    myModule = module;
+  }
+
+  @NotNull
+  @Override
+  public String getId() {
+    return "go.libraries.configurable";
+  }
+
+  @Nullable
+  @Override
+  public Runnable enableSearch(String s) {
+    return null;
+  }
+
+  @Nls
+  @Override
+  public String getDisplayName() {
+    return "Go Libraries";
+  }
+
+  @Nullable
+  @Override
+  public String getHelpTopic() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public JComponent createComponent() {
+    return new JPanel();
+  }
+
+  @Override
+  public boolean isModified() {
+    return false;
+  }
+
+  @Override
+  public void apply() throws ConfigurationException {
+
+  }
+
+  @Override
+  public void reset() {
+
+  }
+
+  @Override
+  public void disposeUIResources() {
+
+  }
+}
