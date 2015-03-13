34b9c8c4150c5db4298b979eef20740806684eb6
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-03-13 10:32:18 +0300

    cleanup

--- src/com/goide/psi/impl/GoTypeReference.java
+++ src/com/goide/psi/impl/GoTypeReference.java
@@ -80,10 +80,9 @@ public class GoTypeReference extends PsiPolyVariantReferenceBase<GoTypeReference
   @Override
   @NotNull
   public ResolveResult[] multiResolve(final boolean incompleteCode) {
-    if (!myElement.isValid()) {
-      return ResolveResult.EMPTY_ARRAY;
-    }
-    return ResolveCache.getInstance(myElement.getProject()).resolveWithCaching(this, MY_RESOLVER, false, false);
+    return myElement.isValid()
+           ? ResolveCache.getInstance(myElement.getProject()).resolveWithCaching(this, MY_RESOLVER, false, false)
+           : ResolveResult.EMPTY_ARRAY;
   }
 
   @NotNull
