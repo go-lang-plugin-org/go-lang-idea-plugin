63c565fc6562caccbeb931fb62c93f9ce4798fea
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-03-19 16:19:29 +0100

    Create core app engine module. Set dependencies

---
+++ google-app-engine/src/com/intellij/appengine/GoogleAppEngineIcons.java
@@ -0,0 +1,9 @@
+package com.intellij.appengine;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class GoogleAppEngineIcons {
+  public static final Icon AppEngine = IconLoader.findIcon("/icons/appEngine.png");
+}
