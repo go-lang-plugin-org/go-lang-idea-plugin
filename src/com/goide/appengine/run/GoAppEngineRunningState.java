5ae5ec7009ed332e7051e28fe36ac004435c64a4
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-03-30 12:21:46 +0300

    Add console filter with util method

--- src/com/goide/appengine/run/GoAppEngineRunningState.java
+++ src/com/goide/appengine/run/GoAppEngineRunningState.java
@@ -4,31 +4,19 @@ import com.goide.runconfig.GoConsoleFilter;
 import com.goide.runconfig.GoRunningState;
 import com.goide.util.GoExecutor;
 import com.intellij.execution.ExecutionException;
-import com.intellij.execution.Executor;
 import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.ui.ConsoleView;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class GoAppEngineRunningState extends GoRunningState<GoAppEngineRunConfiguration> {
   public GoAppEngineRunningState(@NotNull ExecutionEnvironment env,
                                  @NotNull Module module,
                                  @NotNull GoAppEngineRunConfiguration configuration) {
     super(env, module, configuration);
+    addConsoleFilters(new GoConsoleFilter(myConfiguration.getProject(), myModule, myConfiguration.getWorkingDirectory()));
   }
   
-  @Nullable
-  @Override
-  protected ConsoleView createConsole(@NotNull Executor executor) throws ExecutionException {
-    ConsoleView consoleView = super.createConsole(executor);
-    if (consoleView != null) {
-      consoleView.addMessageFilter(new GoConsoleFilter(myConfiguration.getProject(), myModule, myConfiguration.getWorkingDirectory()));
-    }
-    return consoleView;
-  }
-
   @Override
   protected GoExecutor patchExecutor(@NotNull GoExecutor executor) throws ExecutionException {
     executor.withParameters("serve");
