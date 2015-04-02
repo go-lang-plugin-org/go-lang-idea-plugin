0bcfd659afb07af4f97467c83e78549346377de4
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-04-03 02:31:37 +0300

    fix wrong sdk path

--- src/com/goide/sdk/GoSdkService.java
+++ src/com/goide/sdk/GoSdkService.java
@@ -75,7 +75,7 @@ public abstract class GoSdkService extends SimpleModificationTracker {
         
         boolean gcloudInstallation = sdkHomePath.endsWith(GoConstants.GCLOUD_APP_ENGINE_DIRECTORY_PATH);
         if (gcloudInstallation) {
-          goExecutablePath = FileUtil.join(StringUtil.trimEnd(sdkHomePath, GoConstants.GCLOUD_APP_ENGINE_DIRECTORY_PATH), "bin");
+          goExecutablePath = FileUtil.join(StringUtil.trimEnd(goExecutablePath, GoConstants.GCLOUD_APP_ENGINE_DIRECTORY_PATH), "bin");
         }
         return FileUtil.join(goExecutablePath, GoEnvironmentUtil.getGaeExecutableFileName(gcloudInstallation));
       }
