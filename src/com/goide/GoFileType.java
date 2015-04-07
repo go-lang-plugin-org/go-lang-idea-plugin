70ae0186e51653d58cc5c59f12185d39ef43a686
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-04-07 20:30:45 +0300

    Unmodifiable UTF-8 encoding for go files
    Fixes #1537

--- src/com/goide/GoFileType.java
+++ src/com/goide/GoFileType.java
@@ -17,10 +17,14 @@
 package com.goide;
 
 import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.nio.charset.Charset;
 
 public class GoFileType extends LanguageFileType {
   public static final LanguageFileType INSTANCE = new GoFileType();
@@ -51,5 +55,15 @@ public class GoFileType extends LanguageFileType {
   @Override
   public Icon getIcon() {
     return GoIcons.ICON;
+  }
+
+  @Override
+  public String getCharset(@NotNull VirtualFile file, @NotNull byte[] content) {
+    return CharsetToolkit.UTF8;
+  }
+
+  @Override
+  public Charset extractCharsetFromFileContent(@Nullable Project project, @Nullable VirtualFile file, @NotNull CharSequence content) {
+    return CharsetToolkit.UTF8_CHARSET;
   }
 }
