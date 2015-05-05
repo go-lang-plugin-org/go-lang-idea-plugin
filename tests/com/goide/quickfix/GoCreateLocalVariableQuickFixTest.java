3df63a4ab2846dfe54f81279b636d84d4af12cec
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-05-05 18:56:53 +0300

    Cleanup

--- tests/com/goide/quickfix/GoCreateLocalVariableQuickFixTest.java
+++ tests/com/goide/quickfix/GoCreateLocalVariableQuickFixTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Mihai Toader, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.goide.quickfix;
 
 import com.goide.inspections.unresolved.GoUnresolvedReferenceInspection;
@@ -6,13 +22,12 @@ public class GoCreateLocalVariableQuickFixTest extends GoQuickFixTestBase {
   @Override
   protected void setUp() throws Exception {
     super.setUp();
-    //noinspection unchecked
     myFixture.enableInspections(GoUnresolvedReferenceInspection.class);
   }
 
   @Override
-  protected String getTestDataPath() {
-    return "testData/quickfixes/local-variable/";
+  protected String getBasePath() {
+    return "quickfixes/local-variable";
   }
 
   public void testSimple() { doTest("Create local variable 'a'"); }
