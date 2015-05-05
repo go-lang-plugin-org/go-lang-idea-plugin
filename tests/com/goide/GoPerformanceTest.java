bda0f8258079e27e7e258abb289905b26f89eadc
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-05-05 14:48:12 +0300

    Update perfromance expectations

--- tests/com/goide/GoPerformanceTest.java
+++ tests/com/goide/GoPerformanceTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Mihai Toader, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.goide;
 
 import com.goide.inspections.GoUnusedImportDeclaration;
@@ -57,15 +73,15 @@ public class GoPerformanceTest extends GoCodeInsightFixtureTestCase {
   }
 
   public void testPerformanceA() {
-    doHighlightingTest();
+    doHighlightingTest(TimeUnit.SECONDS.toMillis(10));
   }
 
   public void testPerformanceA2() {
-    doHighlightingTest();
+    doHighlightingTest(TimeUnit.SECONDS.toMillis(15));
   }
 
-  private void doHighlightingTest() {
-    PlatformTestUtil.startPerformanceTest(getTestName(true), 10 * 1000, new ThrowableRunnable() {
+  private void doHighlightingTest(long expectation) {
+    PlatformTestUtil.startPerformanceTest(getTestName(true), (int)expectation, new ThrowableRunnable() {
       @Override
       public void run() throws Throwable {
         myFixture.testHighlighting(true, false, false, getTestName(true) + ".go");
