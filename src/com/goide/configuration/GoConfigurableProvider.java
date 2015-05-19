3c7ec20650ba5e1c3a79b37357c507511fdbca4c
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-05-20 01:10:41 +0300

    Cleanup configurable provider. There are always more than one child

--- src/com/goide/configuration/GoConfigurableProvider.java
+++ src/com/goide/configuration/GoConfigurableProvider.java
@@ -20,14 +20,11 @@ import com.goide.project.GoBuildTargetSettings;
 import com.goide.sdk.GoSdkService;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurableProvider;
-import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SearchableConfigurable;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
 
 public class GoConfigurableProvider extends ConfigurableProvider {
   @NotNull private final Project myProject;
@@ -51,7 +48,6 @@ public class GoConfigurableProvider extends ConfigurableProvider {
   }
 
   private static class GoCompositeConfigurable extends SearchableConfigurable.Parent.Abstract {
-
     private Configurable[] myConfigurables;
 
     public GoCompositeConfigurable(Configurable... configurables) {
@@ -60,42 +56,7 @@ public class GoConfigurableProvider extends ConfigurableProvider {
 
     @Override
     protected Configurable[] buildConfigurables() {
-      return myConfigurables.length == 1 ? new Configurable[0] : myConfigurables;
-    }
-
-    @Override
-    public boolean isModified() {
-      return myConfigurables.length == 1 ? myConfigurables[0].isModified() : super.isModified();
-    }
-
-    @Override
-    public void apply() throws ConfigurationException {
-      if (myConfigurables.length == 1) {
-        myConfigurables[0].apply();
-      }
-      else {
-        super.apply();
-      }
-    }
-
-    @Override
-    public void reset() {
-      if (myConfigurables.length == 1) {
-        myConfigurables[0].reset();
-      }
-      else {
-        super.reset();
-      }
-    }
-
-    @Override
-    public JComponent createComponent() {
-      return myConfigurables.length == 1 ? myConfigurables[0].createComponent() : super.createComponent();
-    }
-
-    @Override
-    public boolean hasOwnContent() {
-      return myConfigurables.length == 1;
+      return myConfigurables;
     }
 
     @NotNull
@@ -107,7 +68,7 @@ public class GoConfigurableProvider extends ConfigurableProvider {
     @Nls
     @Override
     public String getDisplayName() {
-      return myConfigurables.length == 1 ? myConfigurables[0].getDisplayName() : "Go";
+      return "Go";
     }
 
     @Nullable
