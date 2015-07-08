9892bdff484062ed001e05f3b5c3f8f5a3d8ff1a
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-07-08 15:09:26 -0600

    extract GoCaseClause

--- gen/com/goide/psi/GoExprCaseClause.java
+++ gen/com/goide/psi/GoExprCaseClause.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.psi.PsiElement;
 
-public interface GoExprCaseClause extends GoCompositeElement {
+public interface GoExprCaseClause extends GoCaseClause {
 
   @NotNull
   List<GoExpression> getExpressionList();
--- gen/com/goide/psi/GoTypeCaseClause.java
+++ gen/com/goide/psi/GoTypeCaseClause.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.psi.PsiElement;
 
-public interface GoTypeCaseClause extends GoCompositeElement {
+public interface GoTypeCaseClause extends GoCaseClause {
 
   @NotNull
   List<GoStatement> getStatementList();
