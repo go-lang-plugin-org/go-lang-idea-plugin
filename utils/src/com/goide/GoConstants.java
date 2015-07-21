ace214667b9ef6712e9f35ff1085c76d372e5c91
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-07-21 14:02:34 +0300

    Update platforms list

--- utils/src/com/goide/GoConstants.java
+++ utils/src/com/goide/GoConstants.java
@@ -72,14 +72,17 @@ public class GoConstants {
   // see "$GOROOT/src/go/build/syslist.go
   public static final Set<String> KNOWN_OS = ContainerUtil.immutableSet("android", "darwin", "dragonfly", "freebsd", "linux", "nacl",
                                                                         "netbsd", "openbsd", "plan9", "solaris", "windows");
-  public static final Set<String> KNOWN_ARCH = ContainerUtil.immutableSet("386", "amd64", "amd64p32", "arm", "arm64");
+  public static final Set<String> KNOWN_ARCH = ContainerUtil.immutableSet("386", "amd64", "amd64p32", "arm", "armbe", "arm64", "arm64be", 
+                                                                          "ppc64", "ppc64le", "mips", "mipsle", "mips64", "mips64le", 
+                                                                          "mips64p32", "mips64p32le", "ppc", "s390", "s390x", "sparc", 
+                                                                          "sparc64");
   public static final Set<String> KNOWN_VERSIONS = ContainerUtil.immutableSet("go1.1", "go1.2", "go1.3", "go1.4");
   public static final Set<String> KNOWN_CGO = ContainerUtil.immutableSet("darwin/386", "darwin/amd64", "dragonfly/386", "dragonfly/amd64",
                                                                          "freebsd/386", "freebsd/amd64", "freebsd/arm", "linux/386",
                                                                          "linux/amd64", "linux/arm", "linux/arm64", "android/386", 
                                                                          "android/amd64", "android/arm", "netbsd/386", "netbsd/amd64", 
                                                                          "netbsd/arm", "openbsd/386", "openbsd/amd64", "windows/386", 
-                                                                         "windows/amd64");
+                                                                         "windows/amd64", "linux/ppc64le");
   public static final Set<String> KNOWN_COMPILERS = ContainerUtil.immutableSet("gc", "gccgo");
 
   private GoConstants() {
