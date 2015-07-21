3fc2cfa324bd860053f25b4e3a849776aef2afb0
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2015-07-21 15:18:02 +0300

    Fix result count for 'make', 'new' functions (fixes #1760)

--- src/com/goide/inspections/GoInspectionUtil.java
+++ src/com/goide/inspections/GoInspectionUtil.java
@@ -30,7 +30,7 @@ public class GoInspectionUtil {
 
   public static int getExpressionResultCount(GoExpression call) {
     if (call instanceof GoLiteral || call instanceof GoBinaryExpr || call instanceof GoParenthesesExpr ||
-        (call instanceof GoUnaryExpr && ((GoUnaryExpr)call).getSendChannel() == null)) {
+        (call instanceof GoUnaryExpr && ((GoUnaryExpr)call).getSendChannel() == null) || call instanceof GoBuiltinCallExpr) {
       return 1;
     }
     else if (call instanceof GoTypeAssertionExpr) {
