0b78860b1b63dbb0c07796cce439bdcc8cea0ad4
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2015-07-28 16:24:01 +0300

    Can't Use "nil" to Initialize a Variable Without an Explicit Type (fixes # 1758)

---
+++ src/com/goide/inspections/GoAssignmentNilWithoutExplicitType.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Mihai Toader, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.goide.inspections;
+
+import com.goide.psi.*;
+import com.intellij.codeInspection.LocalInspectionToolSession;
+import com.intellij.codeInspection.ProblemsHolder;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static com.intellij.codeInspection.ProblemHighlightType.GENERIC_ERROR_OR_WARNING;
+
+public class GoAssignmentNilWithoutExplicitType extends GoInspectionBase {
+  @NotNull
+  @Override
+  protected GoVisitor buildGoVisitor(@NotNull final ProblemsHolder holder,
+                                     @SuppressWarnings({"UnusedParameters", "For future"}) @NotNull LocalInspectionToolSession session) {
+    return new GoVisitor() {
+      @Override
+      public void visitStatement(@NotNull GoStatement o) {
+        GoVarDeclaration declaration = o.getVarDeclaration();
+        if (declaration != null) {
+          List<GoVarSpec> specs = declaration.getVarSpecList();
+          for (GoVarSpec spec : specs) {
+
+            if (spec.getType() != null)
+              continue;
+
+            List<GoExpression> expressions = spec.getExpressionList();
+            for (GoExpression expr : expressions) {
+              if (expr instanceof GoReferenceExpression) {
+                String name = ((GoReferenceExpression)expr).getIdentifier().getText();
+                if (name != null && name.equals("nil")) {
+                  holder.registerProblem(expr, "Cannot assign nil without explicit type", GENERIC_ERROR_OR_WARNING);
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+  }
+}
--- tests/com/goide/inspections/GoHighlightingTest.java
+++ tests/com/goide/inspections/GoHighlightingTest.java
@@ -51,7 +51,8 @@ public class GoHighlightingTest extends GoCodeInsightFixtureTestCase {
       GoReservedWordUsedAsName.class,
       GoMultiplePackagesInspection.class,
       GoCgoInTestInspection.class,
-      GoTestSignaturesInspection.class
+      GoTestSignaturesInspection.class,
+      GoAssignmentNilWithoutExplicitType.class
     );
   }
 
@@ -203,6 +204,12 @@ public class GoHighlightingTest extends GoCodeInsightFixtureTestCase {
 
   public void testDuplicateBuiltinType() {
     myFixture.configureByText("a.go", "package main; func main() {<warning descr=\"Variable 'string' collides with builtin type\">string</warning> := 3; _ = string}");
+    myFixture.checkHighlighting();
+  }
+
+  public void testUseNilWithoutExplicitType() {
+    myFixture.configureByText("a.go", "package main; func main() { var x = <error>nil</error>; _ = x}");
+    myFixture.configureByText("b.go", "package main; func main() { var x string = nil; _ = x}");
     myFixture.checkHighlighting();
   }
 
