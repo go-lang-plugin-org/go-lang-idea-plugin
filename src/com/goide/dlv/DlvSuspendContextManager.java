0e113d44b9d74432ea42ef8f1dd9f5abd52cfbe1
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-07-29 11:42:01 +0300

    get rid of bunch overcomplicated implementations

--- src/com/goide/dlv/DlvSuspendContextManager.java
+++ src/com/goide/dlv/DlvSuspendContextManager.java
@@ -16,26 +16,12 @@
 
 package com.goide.dlv;
 
-import com.goide.dlv.rdp.*;
-import com.intellij.util.Consumer;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.concurrency.AsyncFunction;
-import org.jetbrains.concurrency.AsyncPromise;
 import org.jetbrains.concurrency.Promise;
-import org.jetbrains.debugger.CallFrame;
-import org.jetbrains.debugger.DebugEventListener;
-import org.jetbrains.debugger.StepAction;
-import org.jetbrains.debugger.SuspendContextManagerBase;
-import org.jetbrains.rpc.CommandProcessor;
+import org.jetbrains.debugger.*;
 
-import java.util.concurrent.atomic.AtomicReference;
-
-public class DlvSuspendContextManager extends SuspendContextManagerBase<DlvSuspendContext, DlvCallFrame> {
+public class DlvSuspendContextManager extends SuspendContextManagerBase<SuspendContextBase, CallFrame> {
   @NotNull private final DlvVm vm;
-
-  final AtomicReference<AsyncPromise<CompletionValueYetAnotherPoorFirefoxRdpStructure>> clientEvaluate =
-    new AtomicReference<AsyncPromise<CompletionValueYetAnotherPoorFirefoxRdpStructure>>();
 
   DlvSuspendContextManager(@NotNull DlvVm vm) {
     this.vm = vm;
@@ -47,79 +33,15 @@ public class DlvSuspendContextManager extends SuspendContextManagerBase<DlvSuspe
     return vm.getDebugListener();
   }
 
-  public void mainFrameUpdated() {
-    try {
-      dismissContext();
-    }
-    finally {
-      AsyncPromise<CompletionValueYetAnotherPoorFirefoxRdpStructure> promise = clientEvaluate.getAndSet(null);
-      if (promise != null) {
-        promise.setError(Promise.createError("Frame updated"));
-      }
-    }
-  }
-
-  public void paused(@NotNull final ThreadInterrupted event) {
-    if (event.why().type() == ThreadInterrupted.Reason.Type.CLIENT_EVALUATED) {
-      AsyncPromise<CompletionValueYetAnotherPoorFirefoxRdpStructure> promise = clientEvaluate.getAndSet(null);
-      CommandProcessor.LOG.assertTrue(promise != null);
-      CompletionValueYetAnotherPoorFirefoxRdpStructure evaluatedResult = event.why().frameFinished();
-      CommandProcessor.LOG.assertTrue(evaluatedResult != null);
-      if (evaluatedResult.terminated()) {
-        promise.setError(Promise.createError("Terminated"));
-      }
-      else {
-        promise.setResult(evaluatedResult);
-      }
-      return;
-    }
-
-    final Frame topFrame = event.frame();
-    if (topFrame == null) {
-      vm.commandProcessor.send(DlvRequest.getFrames(vm.threadActor))
-        .done(new Consumer<FramesResult>() {
-          @Override
-          public void consume(@NotNull FramesResult result) {
-            DlvValueManager valueManager = new DlvValueManager(vm);
-            CallFrame[] frames = DlvSuspendContext.createFrames(result, null, valueManager);
-            doPaused(event, null, frames);
-          }
-        });
-    }
-    else {
-      doPaused(event, topFrame, null);
-    }
-  }
-
-  private void doPaused(@NotNull ThreadInterrupted event, @Nullable Frame topFrame, @Nullable CallFrame[] frames) {
-    AsyncPromise<Void> callback = suspendCallback.getAndSet(null);
-    DlvSuspendContext context = new DlvSuspendContext(topFrame, frames, new DlvValueManager(vm), event, callback != null);
-    setContext(context);
-
-    if (callback != null) {
-      callback.setResult(null);
-    }
-
-    vm.getDebugListener().suspended(context);
-  }
-
   @NotNull
   @Override
   protected Promise<?> doSuspend() {
-    return vm.commandProcessor.send(DlvRequest.interrupt(vm.threadActor));
+    return Promise.resolve(null);
   }
 
   @NotNull
   @Override
   public Promise<Void> continueVm(@NotNull final StepAction stepAction, int stepCount) {
-    return getContextOrFail().getValueManager().release()
-      .then(new AsyncFunction<Void, Void>() {
-        @NotNull
-        @Override
-        public Promise<Void> fun(Void aVoid) {
-          return dismissContextOnDone(vm.commandProcessor.send(
-            DlvRequest.resumeThread(vm.threadActor, stepAction, true))); // todo: vm.commandProcessor.isBreakOnException()
-        }
-      });
+    return Promise.DONE;
   }
 }
