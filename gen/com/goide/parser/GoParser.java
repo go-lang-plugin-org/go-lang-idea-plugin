5727c2602cbb41aad415822a2b83297d8da425f3
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-08-21 13:43:09 +0300

    regen with the latest GK

--- gen/com/goide/parser/GoParser.java
+++ gen/com/goide/parser/GoParser.java
@@ -9,9 +9,10 @@ import com.intellij.psi.tree.IElementType;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.lang.PsiParser;
+import com.intellij.lang.LightPsiParser;
 
 @SuppressWarnings({"SimplifiableIfStatement", "UnusedAssignment"})
-public class GoParser implements PsiParser {
+public class GoParser implements PsiParser, LightPsiParser {
 
   public ASTNode parse(IElementType t, PsiBuilder b) {
     parseLight(t, b);
