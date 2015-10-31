0fd456c362b01f6c260645d90fb59840c27e664a
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-10-31 18:38:14 +0300

    dlv API: suppress unused fields inspection

--- src/com/goide/dlv/protocol/DlvApi.java
+++ src/com/goide/dlv/protocol/DlvApi.java
@@ -20,6 +20,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+@SuppressWarnings("unused")
 public class DlvApi {
   // DebuggerState represents the current context of the debugger.
   public static class DebuggerState {
