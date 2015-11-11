eea3b3a697571bd4c00ec984d7393a5e332a880e
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-11-11 13:59:53 +0100

    int type for hex literal

--- src/com/goide/psi/impl/GoPsiImplUtil.java
+++ src/com/goide/psi/impl/GoPsiImplUtil.java
@@ -416,7 +416,7 @@ public class GoPsiImplUtil {
     else if (o instanceof GoLiteral) {
       GoLiteral l = (GoLiteral)o;
       if (l.getChar() != null) return getBuiltinType(o, "rune");
-      if (l.getInt() != null) return getBuiltinType(o, "int");
+      if (l.getInt() != null || l.getHex() != null) return getBuiltinType(o, "int");
       if (l.getFloat() != null) return getBuiltinType(o, "float64");
       if (l.getFloati() != null) return getBuiltinType(o, "complex64");
       if (l.getDecimali() != null) return getBuiltinType(o, "complex128");
