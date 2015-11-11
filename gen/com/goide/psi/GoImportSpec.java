1ecdeaeae4a003f149b5e9125c97260994687b37
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-11-11 15:38:37 +0100

    regen
    (cherry picked from commit 3f4e066a9968121c2cfdcd47d3b25fec121ebc93)

--- gen/com/goide/psi/GoImportSpec.java
+++ gen/com/goide/psi/GoImportSpec.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // This is a generated file. Not intended for manual editing.
 package com.goide.psi;
 
@@ -49,7 +33,6 @@ public interface GoImportSpec extends GoNamedElement, StubBasedPsiElement<GoImpo
 
   String getName();
 
-  @NotNull
   boolean isCImport();
 
 }
