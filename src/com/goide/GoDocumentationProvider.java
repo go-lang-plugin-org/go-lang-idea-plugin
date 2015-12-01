cf3bb3a3ac086a25208ea7fcdb36b5654105d6f5
Alexander Zolotov <goldifit@gmail.com>
2015-12-01 16:53:12 +0300

    Fix nullability

--- src/com/goide/GoDocumentationProvider.java
+++ src/com/goide/GoDocumentationProvider.java
@@ -138,7 +138,8 @@ public class GoDocumentationProvider extends AbstractDocumentationProvider {
     return null;
   }
 
-  private static PsiElement adjustDocElement(PsiElement element) {
+  @Nullable
+  private static PsiElement adjustDocElement(@Nullable PsiElement element) {
     return element instanceof GoImportSpec ? ((GoImportSpec)element).getImportString().resolve() : element;
   }
 
@@ -280,7 +281,7 @@ public class GoDocumentationProvider extends AbstractDocumentationProvider {
   }
 
   @Nullable
-  private static String getReferenceText(@NotNull PsiElement element) {
+  private static String getReferenceText(@Nullable PsiElement element) {
     if (element instanceof GoNamedElement) {
       PsiFile file = element.getContainingFile();
       if (file instanceof GoFile) {
