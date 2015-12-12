6e66ec4b3cb52be38a2f65b05eb9d1fc14f17818
Alexander Zolotov <goldifit@gmail.com>
2015-12-12 07:38:51 +0300

    Introduce getType methods for var and const definitions (cherry picked from commit 5125782)

--- gen/com/goide/psi/impl/GoConstDefinitionImpl.java
+++ gen/com/goide/psi/impl/GoConstDefinitionImpl.java
@@ -1,17 +1,35 @@
+/*
+ * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // This is a generated file. Not intended for manual editing.
 package com.goide.psi.impl;
 
-import java.util.List;
-import org.jetbrains.annotations.*;
+import com.goide.psi.GoConstDefinition;
+import com.goide.psi.GoType;
+import com.goide.psi.GoVisitor;
+import com.goide.stubs.GoConstDefinitionStub;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.util.PsiTreeUtil;
-import static com.goide.GoTypes.*;
-import com.goide.stubs.GoConstDefinitionStub;
-import com.goide.psi.*;
 import com.intellij.psi.ResolveState;
 import com.intellij.psi.stubs.IStubElementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static com.goide.GoTypes.IDENTIFIER;
 
 public class GoConstDefinitionImpl extends GoNamedElementImpl<GoConstDefinitionStub> implements GoConstDefinition {
 
@@ -41,6 +59,11 @@ public class GoConstDefinitionImpl extends GoNamedElementImpl<GoConstDefinitionS
   @Nullable
   public GoType getGoTypeInner(ResolveState context) {
     return GoPsiImplUtil.getGoTypeInner(this, context);
+  }
+
+  @Nullable
+  public GoType getType() {
+    return GoPsiImplUtil.getType(this);
   }
 
 }
