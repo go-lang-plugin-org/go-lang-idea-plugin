fbae253b47840d3035b4bff6ba7f5e25a6132507
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-12-17 18:28:22 +0300

    [resolve] tweak cgo processing, #2078

--- testData/highlighting/cgotest.go
+++ testData/highlighting/cgotest.go
@@ -19,6 +19,10 @@ int x;
 unsigned char y;
 char *name;
 } S;
+
+S* some_C_function_call() {
+	return 0;
+}
 */
 import "C"
 
@@ -28,6 +32,11 @@ var s C.struct_x
 var s1 *C.struct_x
 
 func main() {
+	s2 := C.some_C_function_call()
+	if (false) {
+		fmt.Println(s2.x)
+	}
+
 	a := &X{5, 7}
 	fmt.Println(s.y)
 	fmt.Println(s1)
