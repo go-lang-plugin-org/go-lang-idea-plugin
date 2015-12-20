e90a0599df2de0368c475cb4d75b96a2a264979e
James Ring <sjr@google.com>
2015-12-19 16:43:39 -0800

    Ignore embedded types when finding duplicate methods
    Fixes https://github.com/go-lang-plugin-org/go-lang-idea-plugin/issues/2099.

--- tests/com/goide/inspections/GoHighlightingTest.java
+++ tests/com/goide/inspections/GoHighlightingTest.java
@@ -125,6 +125,7 @@ public class GoHighlightingTest extends GoCodeInsightFixtureTestCase {
   public void testEqualinif()                 { doTest(); }
   public void testSpecTypes()                 { doTest(); }
   public void testFunctionTypes()             { doTest(); }
+  public void testGithubIssue2099()           { doTest(); }
 
   public void testRelativeImportIgnoringDirectories() throws IOException {
     myFixture.getTempDirFixture().findOrCreateDir("to_import/testdata");
