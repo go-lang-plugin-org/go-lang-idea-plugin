bea1ae74c76fd415f049657abccb618f005e179c
Alexander Zolotov <goldifit@gmail.com>
2015-12-24 00:20:21 +0300

    Simplify annotator

--- testData/colorHighlighting/octAndHex.go
+++ testData/colorHighlighting/octAndHex.go
@@ -1,5 +1,5 @@
 package main
 
-var <info descr="null"><info descr="var">a</info></info> int = <info descr="null"><info descr="hex_oct">0112</info></info>
-var <info descr="null"><info descr="var">b</info></info> int = <info descr="null"><info descr="hex_oct">0x112</info></info>
-var <info descr="null"><info descr="var">c</info></info> int = <info descr="null"><info descr="hex_oct">0Xabf112</info></info>
+var <info descr="null"><info descr="GO_PACKAGE_LOCAL_VARIABLE">a</info></info> int = <info descr="null"><info descr="GO_NUMBER">0112</info></info>
+var <info descr="null"><info descr="GO_PACKAGE_LOCAL_VARIABLE">b</info></info> int = <info descr="null"><info descr="GO_NUMBER">0x112</info></info>
+var <info descr="null"><info descr="GO_PACKAGE_LOCAL_VARIABLE">c</info></info> int = <info descr="null"><info descr="GO_NUMBER">0Xabf112</info></info>
--- testData/colorHighlighting/types.go
+++ testData/colorHighlighting/types.go
@@ -2,24 +2,24 @@ package main
 
 type (
 	<info descr="null"><info descr="GO_PACKAGE_EXPORTED_INTERFACE">PublicInterface</info></info>  interface {
-		<info descr="null"><info descr="signature_owner">PublicFunc</info></info>() int
-		<info descr="null"><info descr="signature_owner">privateFunc</info></info>() int
+		<info descr="null"><info descr="GO_EXPORTED_FUNCTION">PublicFunc</info></info>() int
+		<info descr="null"><info descr="GO_LOCAL_FUNCTION">privateFunc</info></info>() int
 	}
 
 	<info descr="null"><info descr="GO_PACKAGE_LOCAL_INTERFACE">private</info></info> interface {
-		<info descr="null"><info descr="signature_owner">PublicFunc</info></info>() int
-		<info descr="null"><info descr="signature_owner">privateFunc</info></info>() int
+		<info descr="null"><info descr="GO_EXPORTED_FUNCTION">PublicFunc</info></info>() int
+		<info descr="null"><info descr="GO_LOCAL_FUNCTION">privateFunc</info></info>() int
 	}
 
 	<info descr="null"><info descr="GO_PACKAGE_EXPORTED_STRUCT">PublicStruct</info></info> struct {
 
-		<info descr="null"><info descr="field">PublicField</info></info>  int
-		<info descr="null"><info descr="field">privateField</info></info> int
+		<info descr="null"><info descr="GO_STRUCT_EXPORTED_MEMBER">PublicField</info></info>  int
+		<info descr="null"><info descr="GO_STRUCT_LOCAL_MEMBER">privateField</info></info> int
 	}
 
 	<info descr="null"><info descr="GO_PACKAGE_LOCAL_STRUCT">privateStruct</info></info> struct {
-		<info descr="null"><info descr="field">PublicField</info></info>  int
-		<info descr="null"><info descr="field">privateField</info></info> int
+		<info descr="null"><info descr="GO_STRUCT_EXPORTED_MEMBER">PublicField</info></info>  int
+		<info descr="null"><info descr="GO_STRUCT_LOCAL_MEMBER">privateField</info></info> int
 	}
 
 	<info descr="null"><info descr="GO_TYPE_SPECIFICATION">demoInt</info></info> int
