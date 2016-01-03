3541cf5911b2910e057895444e5c8aa73e988c16
Alexander Zolotov <goldifit@gmail.com>
2016-01-04 02:47:31 +0300

    [completion] do not complete keywords inside const spec

--- tests/com/goide/completion/GoCompletionTest.java
+++ tests/com/goide/completion/GoCompletionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -232,6 +232,10 @@ public class GoCompletionTest extends GoCompletionTestBase {
   public void testForStatementKeywords() {
     myFixture.testCompletionVariants(getTestName(true) + ".go", "bar", "break", "const", "continue", "defer", "for", "go", "if", "return",
                                      "fallthrough", "goto", "select", "switch", "var", "struct", "map");
+  }
+
+  public void testDoNotCompleteKeywordsInsideConstSpec() {
+    myFixture.testCompletionVariants(getTestName(true) + ".go", "a");
   }
 
   public void testChanKeyword() {
