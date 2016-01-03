70cdfde37fce0c0de04c795727a4a666c886e0bb
Alexander Zolotov <goldifit@gmail.com>
2016-01-04 02:23:28 +0300

    private (cherry picked from commit 787f514)

--- src/com/goide/completion/GoKeywordCompletionProvider.java
+++ src/com/goide/completion/GoKeywordCompletionProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,10 +55,10 @@ public class GoKeywordCompletionProvider extends CompletionProvider<CompletionPa
     this(priority, insertHandler, null, keywords);
   }
 
-  public GoKeywordCompletionProvider(int priority,
-                                     @Nullable InsertHandler<LookupElement> insertHandler,
-                                     @Nullable AutoCompletionPolicy completionPolicy,
-                                     @NotNull String... keywords) {
+  private GoKeywordCompletionProvider(int priority,
+                                      @Nullable InsertHandler<LookupElement> insertHandler,
+                                      @Nullable AutoCompletionPolicy completionPolicy,
+                                      @NotNull String... keywords) {
     myPriority = priority;
     myInsertHandler = insertHandler;
     myCompletionPolicy = completionPolicy;
