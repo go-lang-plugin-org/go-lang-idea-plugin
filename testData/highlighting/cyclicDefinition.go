2f8a597c4ce745962a24972c97010b8251057b6c
Alexander Zolotov <goldifit@gmail.com>
2016-01-04 05:00:25 +0300

    [annotator] Get non-strict topmost parent while checking cyclic definition
    Fixes #2096

--- testData/highlighting/cyclicDefinition.go
+++ testData/highlighting/cyclicDefinition.go
@@ -1,5 +1,8 @@
 package main
 
+var Foo1 = <error descr="Cyclic definition detected">Foo1</error>
+const Bar1 = <error descr="Cyclic definition detected">Bar1</error>
+
 var (
 	Foo int = 1 + <error descr="Cyclic definition detected">Foo</error>
 )
