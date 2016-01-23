5e6a1166a56f35ab39fc4ebcebdcadf36698c340
Florin Patan <florinpatan@gmail.com>
2016-01-23 20:40:58 +0100

    Improve inspection for non-local type definitions (fixes #2269)
    (cherry picked from commit d0b47d5271ed517e31d086bc78c984556d03ccef)

--- src/com/goide/inspections/GoMethodOnNonLocalTypeInspection.java
+++ src/com/goide/inspections/GoMethodOnNonLocalTypeInspection.java
@@ -39,7 +39,7 @@ public class GoMethodOnNonLocalTypeInspection extends GoInspectionBase {
         if (methodType == null ||
             methodType.getTypeReferenceExpression() == null) return;
 
-        methodType = GoPsiImplUtil.findBaseTypeFromRef(methodType.getTypeReferenceExpression());
+        methodType = GoPsiImplUtil.findTypeFromTypeRef(methodType.getTypeReferenceExpression());
         if (methodType == null) return;
 
         String typeImportPath = ((GoFile)methodType.getContainingFile()).getImportPath();
