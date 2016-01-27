89dd58fdd3beb829c5c7003db2969a172f83a381
Alexander Zolotov <goldifit@gmail.com>
2016-01-27 14:06:56 +0300

    Always create stubs for improts (cherry picked from commit f95513e)

--- src/com/goide/GoFileElementType.java
+++ src/com/goide/GoFileElementType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ import java.io.IOException;
 
 public class GoFileElementType extends IStubFileElementType<GoFileStub> {
   public static final IStubFileElementType INSTANCE = new GoFileElementType();
-  public static final int VERSION = 14;
+  public static final int VERSION = 15;
 
   private GoFileElementType() {
     super("GO_FILE", GoLanguage.INSTANCE);
