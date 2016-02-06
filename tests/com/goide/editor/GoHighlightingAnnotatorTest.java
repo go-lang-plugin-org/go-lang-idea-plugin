b37b6c25f4990e8701471142ca360775530da952
Alexander Zolotov <goldifit@gmail.com>
2016-02-06 21:45:59 +0300

    Highlight field declaration references
    Fixes #1534
    (cherry picked from commit 52f82f0)

--- tests/com/goide/editor/GoHighlightingAnnotatorTest.java
+++ tests/com/goide/editor/GoHighlightingAnnotatorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,6 +44,10 @@ public class GoHighlightingAnnotatorTest extends GoCodeInsightFixtureTestCase {
   }
   
   public void testTypes() {
+    doTest();
+  }
+  
+  public void testStructFields() {
     doTest();
   }
 
