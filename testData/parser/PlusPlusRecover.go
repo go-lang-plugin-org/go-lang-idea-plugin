3cf7b80ed46975dcf1f6318d20c0d4972b218eb1
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2016-02-06 18:07:33 +0300

    [parser] better recover for statements, don't take '++' and so on into account
    prevent stub mismatch
    fix #2241
    
    (cherry picked from commit 58cf96128833dcef633e911ffcdbb61855d2233f)

---
+++ testData/parser/PlusPlusRecover.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+	i := 0
+	i //error
+	&i //error
+	/i //error
+	/=i //error
+	^i //error
+	<i //error
+	<<i //error
+	=i //error
+	==i //error
+	>i //error
+	>>i //error
+	...i //error
+	!i //error
+	!=i //error
+	%i //error
+	&&i //error
+	&=i //error
+	&^i //error
+	&^=i //error
+	+=i //error
+	--i //error
+	++i //error
+	Println()
+}
