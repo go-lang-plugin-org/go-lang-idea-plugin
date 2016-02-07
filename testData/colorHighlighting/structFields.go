6e5ff15ea645854755caf67eb93c20046bfa121a
Alexander Zolotov <goldifit@gmail.com>
2016-02-08 00:26:49 +0300

    Fix testdata (cherry picked from commit 79e9405)

--- testData/colorHighlighting/structFields.go
+++ testData/colorHighlighting/structFields.go
@@ -1,10 +1,10 @@
 package main
 
 type <info descr="null"><info descr="GO_PACKAGE_LOCAL_STRUCT">type1</info></info> struct {
-	<info descr="null"><info descr="GO_STRUCT_LOCAL_MEMBER">var1</info></info> int
+	<info descr="null"><info descr="GO_STRUCT_LOCAL_MEMBER">var1</info></info> <info descr="null"><info descr="GO_BUILTIN_TYPE_REFERENCE">int</info></info>
 }
 
 func <info descr="null"><info descr="GO_LOCAL_FUNCTION">main</info></info>() {
 	<info descr="null"><info descr="GO_LOCAL_VARIABLE">struct1</info></info> := <info descr="null"><info descr="GO_PACKAGE_LOCAL_STRUCT">type1</info></info>{<info descr="null"><info descr="GO_STRUCT_LOCAL_MEMBER">var1</info></info>: 1}
-	println(<info descr="null"><info descr="GO_LOCAL_VARIABLE">struct1</info></info>.<info descr="null"><info descr="GO_STRUCT_LOCAL_MEMBER">var1</info></info>)
+	<info descr="null"><info descr="GO_BUILTIN_FUNCTION">println</info></info>(<info descr="null"><info descr="GO_LOCAL_VARIABLE">struct1</info></info>.<info descr="null"><info descr="GO_STRUCT_LOCAL_MEMBER">var1</info></info>)
 } 
