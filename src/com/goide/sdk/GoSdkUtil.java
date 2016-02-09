2266ae66079f73088874455f788a7a98bfb14230
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2016-02-09 17:52:47 +0300

    Find subdirectories of libraries by system-independent paths

--- src/com/goide/sdk/GoSdkUtil.java
+++ src/com/goide/sdk/GoSdkUtil.java
@@ -201,6 +201,7 @@ public class GoSdkUtil {
     if (path.isEmpty()) {
       return null;
     }
+    path = FileUtil.toSystemIndependentName(path);
     for (VirtualFile root : getSourcesPathsToLookup(project, module)) {
       VirtualFile file = root.findFileByRelativePath(path);
       if (file != null) {
