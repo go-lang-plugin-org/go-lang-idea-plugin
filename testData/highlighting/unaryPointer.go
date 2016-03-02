8c4f3251cd0f68663a388053ecf5a62e069ebe9b
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2016-03-02 20:06:08 +0300

    [resolve] no need to expand reference type in case of unary pointer expression, fix #2335

---
+++ testData/highlighting/unaryPointer.go
@@ -0,0 +1,21 @@
+package main
+
+type Rat struct {
+	a, b int
+}
+
+
+type Ad struct {
+	Price Rat
+}
+
+func (x *Rat) Cmp(y *Rat) int {
+	return 1
+}
+
+func main() {
+	rat := &Rat{1, 1}
+	i1 := Ad{}.Price
+	i :=  &i1
+	i.Cmp(rat)
+}
