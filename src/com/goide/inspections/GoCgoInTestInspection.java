5b0e70824f347ed6f640758482a9362b218a733a
Alexander Zolotov <goldifit@gmail.com>
2016-03-12 23:56:25 +0300

    Extract delete import quick fix
    (cherry picked from commit 2ab82d4b00ee98badd555f1953bd060b4a62ca65)

--- src/com/goide/inspections/GoCgoInTestInspection.java
+++ src/com/goide/inspections/GoCgoInTestInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package com.goide.inspections;
 
 import com.goide.psi.GoFile;
 import com.goide.psi.GoImportSpec;
-import com.goide.quickfix.GoDeleteQuickFix;
+import com.goide.quickfix.GoDeleteImportQuickFix;
 import com.goide.runconfig.testing.GoTestFinder;
 import com.intellij.codeInspection.ProblemsHolder;
 import org.jetbrains.annotations.NotNull;
@@ -29,7 +29,7 @@ public class GoCgoInTestInspection extends GoInspectionBase {
     if (!GoTestFinder.isTestFile(file)) return;
     for (GoImportSpec importSpec : file.getImports()) {
       if (importSpec.isCImport()) {
-        problemsHolder.registerProblem(importSpec, "Usage of cgo in tests is not supported.", new GoDeleteQuickFix("Remove 'C' import"));
+        problemsHolder.registerProblem(importSpec, "Usage of cgo in tests is not supported.", new GoDeleteImportQuickFix());
       }
     }
   }
