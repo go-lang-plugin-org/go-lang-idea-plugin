b2f3ff45d01b6877883294bd309c8b669c9e0ff5
Alexander Zolotov <goldifit@gmail.com>
2016-03-17 22:40:52 +0300

    [completion] use original file while completion
    Fixes #2352
    
    (cherry picked from commit 6b0d45ffa016fb1cf45ef134f21e30366510a481)

--- src/com/goide/psi/impl/GoPsiImplUtil.java
+++ src/com/goide/psi/impl/GoPsiImplUtil.java
@@ -699,7 +699,7 @@ public class GoPsiImplUtil {
     if (!(file instanceof GoFile) || !GoUtil.allowed(file)) return false;
     // it's not a test or context file is also test from the same package
     return !GoTestFinder.isTestFile(file) ||
-           GoTestFinder.isTestFile(contextFile) && Comparing.equal(file.getParent(), contextFile.getParent());
+           GoTestFinder.isTestFile(contextFile) && Comparing.equal(file.getParent(), contextFile.getOriginalFile().getParent());
   }
 
   static boolean processNamedElements(@NotNull PsiScopeProcessor processor,
