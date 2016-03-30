e9d64efba3346b1c094be2d55cfcb29fada6cf6e
Alexander Zolotov <goldifit@gmail.com>
2016-03-30 23:39:19 +0300

    [quickfix] do not suggest to create local variables and constants on top level
    (cherry picked from commit b05804efe0a5bd01b928da9d4ebffeb9e1d745fe)

--- tests/com/goide/quickfix/GoCreateLocalVariableQuickFixTest.java
+++ tests/com/goide/quickfix/GoCreateLocalVariableQuickFixTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,4 +42,5 @@ public class GoCreateLocalVariableQuickFixTest extends GoQuickFixTestBase {
   public void testIf()         { doTestCreateA(); }
   public void testUnresolved() { doTestCreateA(); }
   public void testResolved()   { doTestCreateA(); }
+  public void testOnTopLevel() { doTestNoFix(CREATE_LOCAL_VARIABLE_A); }
 }
