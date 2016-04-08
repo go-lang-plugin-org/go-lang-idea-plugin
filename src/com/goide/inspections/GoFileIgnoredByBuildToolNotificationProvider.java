fa603e64683c7c59e1e3a77a41bcf5a637c69f83
Alexander Zolotov <goldifit@gmail.com>
2016-04-09 01:38:04 +0300

    Show notification about ignored files

--- src/com/goide/inspections/GoFileIgnoredByBuildToolNotificationProvider.java
+++ src/com/goide/inspections/GoFileIgnoredByBuildToolNotificationProvider.java
@@ -19,6 +19,7 @@ package com.goide.inspections;
 import com.goide.GoFileType;
 import com.goide.project.GoModuleSettings;
 import com.goide.util.GoUtil;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.module.Module;
@@ -41,14 +42,17 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 import java.util.Set;
 
-public class MismatchedBuildTargetNotificationProvider extends EditorNotifications.Provider<EditorNotificationPanel> implements DumbAware {
-  private static final Key<EditorNotificationPanel> KEY = Key.create("Mismatched build target");
+public class GoFileIgnoredByBuildToolNotificationProvider extends EditorNotifications.Provider<EditorNotificationPanel>
+  implements DumbAware {
+
+  private static final Key<EditorNotificationPanel> KEY = Key.create("Go file ignored by build tool");
+  private static final String DO_NOT_SHOW_NOTIFICATION_ABOUT_IGNORE_BY_BUILD_TOOL = "DO_NOT_SHOW_NOTIFICATION_ABOUT_IGNORE_BY_BUILD_TOOL";
 
   private final Project myProject;
 
-  public MismatchedBuildTargetNotificationProvider(@NotNull Project project, 
-                                                   @NotNull final EditorNotifications notifications,
-                                                   @NotNull final FileEditorManager fileEditorManager) {
+  public GoFileIgnoredByBuildToolNotificationProvider(@NotNull Project project,
+                                                      @NotNull final EditorNotifications notifications,
+                                                      @NotNull final FileEditorManager fileEditorManager) {
     myProject = project;
     MessageBusConnection connection = myProject.getMessageBus().connect(myProject);
     connection.subscribe(GoModuleSettings.TOPIC, new GoModuleSettings.BuildTargetListener() {
@@ -84,18 +88,37 @@ public class MismatchedBuildTargetNotificationProvider extends EditorNotificatio
     if (file.getFileType() == GoFileType.INSTANCE) {
       PsiFile psiFile = PsiManager.getInstance(myProject).findFile(file);
       Module module = psiFile != null ? ModuleUtilCore.findModuleForPsiElement(psiFile) : null;
-      if (module != null && !GoUtil.matchedForModuleBuildTarget(psiFile, module)) {
-        return createPanel(module, file);
+      if (GoUtil.fileToIgnore(file.getName())) {
+        if (!PropertiesComponent.getInstance().getBoolean(DO_NOT_SHOW_NOTIFICATION_ABOUT_IGNORE_BY_BUILD_TOOL, false)) {
+          return createIgnoredByBuildToolPanel(myProject, file);
+        }
+      }
+      else if (module != null && !GoUtil.matchedForModuleBuildTarget(psiFile, module)) {
+        return createMismatchedTargetPanel(module, file);
       }
     }
     return null;
   }
 
-  @NotNull
-  private static EditorNotificationPanel createPanel(@NotNull final Module module, @NotNull VirtualFile file) {
+  private static EditorNotificationPanel createIgnoredByBuildToolPanel(@NotNull Project project, @NotNull VirtualFile file) {
     EditorNotificationPanel panel = new EditorNotificationPanel();
-    panel.setText("'" + file.getName() + "' doesn't match to target system");
-    panel.createActionLabel("Open Go project settings", new Runnable() {
+    String fileName = file.getName();
+    panel.setText("'" + fileName + "' will be ignored by build tool since its name starts with '" + fileName.charAt(0) + "'");
+    panel.createActionLabel("Do not show again", new Runnable() {
+      @Override
+      public void run() {
+        PropertiesComponent.getInstance().setValue(DO_NOT_SHOW_NOTIFICATION_ABOUT_IGNORE_BY_BUILD_TOOL, true);
+        EditorNotifications.getInstance(project).updateAllNotifications();
+      }
+    });
+    return panel;
+  }
+
+  @NotNull
+  private static EditorNotificationPanel createMismatchedTargetPanel(@NotNull final Module module, @NotNull VirtualFile file) {
+    EditorNotificationPanel panel = new EditorNotificationPanel();
+    panel.setText("'" + file.getName() + "' doesn't match to target system. File will be ignored by build tool");
+    panel.createActionLabel("Edit Go project settings", new Runnable() {
       @Override
       public void run() {
         GoModuleSettings.showModulesConfigurable(module);
