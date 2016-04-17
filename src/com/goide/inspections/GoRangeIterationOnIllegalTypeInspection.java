6d9517bca1cc71c7227ce8552db244c8fe905f6c
Alexander Zolotov <goldifit@gmail.com>
2016-04-17 16:10:27 +0300

    Simplify range type inspection + fix testdata

--- src/com/goide/inspections/GoRangeIterationOnIllegalTypeInspection.java
+++ src/com/goide/inspections/GoRangeIterationOnIllegalTypeInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,12 +16,15 @@
 
 package com.goide.inspections;
 
-import com.goide.psi.*;
+import com.goide.psi.GoExpression;
+import com.goide.psi.GoRangeClause;
+import com.goide.psi.GoType;
+import com.goide.psi.GoVisitor;
 import com.goide.psi.impl.GoPsiImplUtil;
+import com.goide.psi.impl.GoTypeUtil;
 import com.intellij.codeInspection.LocalInspectionToolSession;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.psi.ResolveState;
 import org.jetbrains.annotations.NotNull;
 
 public class GoRangeIterationOnIllegalTypeInspection extends GoInspectionBase {
@@ -32,43 +35,13 @@ public class GoRangeIterationOnIllegalTypeInspection extends GoInspectionBase {
       @Override
       public void visitRangeClause(@NotNull GoRangeClause o) {
         super.visitRangeClause(o);
-
-        if (o.getExpressionList().size() != 1) return;
-
         GoExpression expression = o.getRangeExpression();
-        if (expression == null || expression instanceof GoStringLiteral) return;
-
-        GoType goType = expression.getGoType(null);
-        if (goType == null) return;
-
-        if (isValidType(goType) || isValidTypeRefExpression(goType)) return;
-
-        holder.registerProblem(expression, "Cannot range over data", ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+        GoType type = expression != null ? expression.getGoType(null) : null;
+        if (type != null && !GoTypeUtil.isIterable(type.getUnderlyingType())) {
+          holder.registerProblem(expression, "Cannot range over data (type " + GoPsiImplUtil.getText(type) + ")", 
+            ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+        }
       }
     };
-  }
-
-  private static boolean isValidType(GoType goType) {
-    if (goType instanceof GoSpecType) {
-      goType = ((GoSpecType)goType).getType();
-    }
-
-    return goType instanceof GoArrayOrSliceType ||
-           goType instanceof GoMapType ||
-           goType instanceof GoChannelType ||
-           (goType.textMatches("string") && GoPsiImplUtil.builtin(goType));
-  }
-
-  private static boolean isValidTypeRefExpression(GoType goType) {
-    if (goType instanceof GoParType) {
-      goType = ((GoParType)goType).getType();
-    }
-
-    if (goType instanceof GoPointerType) {
-      goType = ((GoPointerType)goType).getType();
-      if (goType == null) return false;
-    }
-
-    return isValidType(goType.getUnderlyingType());
   }
 }
