4af82578ff7ec24340089daaa81130498ab636c5
Alexander Zolotov <goldifit@gmail.com>
2016-04-17 17:32:59 +0300

    Cleanup delete quick fix
    (cherry picked from commit 30b10546d36750f33f0bb2e61c2ac3a2ee090706)

--- src/com/goide/quickfix/GoDeleteQuickFix.java
+++ src/com/goide/quickfix/GoDeleteQuickFix.java
@@ -21,22 +21,36 @@ import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class GoDeleteQuickFix extends LocalQuickFixBase {
-  public GoDeleteQuickFix(String name) {
+  private final Class<? extends PsiElement> myClazz;
+  private final IElementType myElementType;
+
+  public GoDeleteQuickFix(@NotNull String name, @NotNull Class<? extends PsiElement> clazz) {
     super(name);
+    myClazz = clazz;
+    myElementType = null;
+  }
+  
+  public GoDeleteQuickFix(@NotNull String name, @NotNull IElementType elementType) {
+    super(name);
+    myClazz = PsiElement.class;
+    myElementType = elementType;
   }
 
   @Override
   public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-    final PsiElement element = descriptor.getPsiElement();
-    if (element == null) return;
-    new WriteCommandAction.Simple(project, getName(), element.getContainingFile()) {
+    WriteCommandAction.runWriteCommandAction(project, new Runnable() {
       @Override
-      protected void run() {
-        element.delete();
+      public void run() {
+        PsiElement element = ObjectUtils.tryCast(descriptor.getStartElement(), myClazz);
+        if (element != null && (myElementType == null || element.getNode().getElementType() == myElementType)) {
+          element.delete();
+        }
       }
-    }.execute();
+    });
   }
 }
