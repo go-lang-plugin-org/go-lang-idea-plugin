ddbdcb00c0630ba274e21602274f5c80b77507a8
Alexander Zolotov <goldifit@gmail.com>
2016-04-22 18:57:08 +0300

    Calculate type for typeSwitch declaration

--- tests/com/goide/type/GoTypeResolveTest.java
+++ tests/com/goide/type/GoTypeResolveTest.java
@@ -17,21 +17,73 @@
 package com.goide.type;
 
 import com.goide.GoCodeInsightFixtureTestCase;
-import com.goide.psi.GoExpression;
 import com.goide.psi.GoType;
+import com.goide.psi.GoTypeOwner;
+import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.testFramework.LightProjectDescriptor;
 import org.jetbrains.annotations.NotNull;
 
 public class GoTypeResolveTest extends GoCodeInsightFixtureTestCase {
-  private void doTest(@NotNull String text, String expected) {
-    myFixture.configureByText("a.go", "package a;" + text);
-    PsiElement element = myFixture.getFile().findElementAt(myFixture.getEditor().getCaretModel().getOffset());
-    GoExpression e = PsiTreeUtil.getParentOfType(element, GoExpression.class);
-    assertNotNull(e);
-    GoType type = e.getGoType(null);
-    assertEquals(expected, type == null ? "<null>" : type.getText());
+  public void testAnon() {
+    doTopLevelTest("type A struct{};type E A;type B struct{ E };func (e E) foo() {};func main() { b := B{}; b.<caret>E }", "E A");
   }
 
-  public void testAnon() { doTest("type A struct{};type E A;type B struct{ E };func (e E) foo() {};func main() { b := B{}; b.<caret>E }", "E A"); }
+  public void testTypeSwitchDeclaration() {
+    doStatementTest("switch fo<caret>o := \"hello\".(type) {}", "string");
+  }
+
+  public void testTypeSwitchUsageInContext() {
+    doStatementTest("switch foo := \"hello\".(type) { case bool:\n println(fo<caret>o)\n}", "bool");
+  }
+
+  public void testNestedTypeSwitchUsageInContext() {
+    doStatementTest("var p interface{}\n" +
+                    "switch foo := p.(type) {\n" +
+                    "case int:\n" +
+                    "  switch p.(type) {\n" +
+                    "  case bool:" +
+                    "    println(f<caret>oo)\n" +
+                    "  }\n" +
+                    "}", "int");
+  }
+
+  private void doTopLevelTest(@NotNull String text, @NotNull String expectedTypeText) {
+    myFixture.configureByText("a.go", "package a;" + text);
+    PsiElement elementAt;
+    SelectionModel selectionModel = myFixture.getEditor().getSelectionModel();
+    if (selectionModel.hasSelection()) {
+      PsiElement left = myFixture.getFile().findElementAt(selectionModel.getSelectionStart());
+      PsiElement right = myFixture.getFile().findElementAt(selectionModel.getSelectionEnd());
+      assertNotNull(left);
+      assertNotNull(right);
+      elementAt = PsiTreeUtil.findCommonParent(left, right);
+    }
+    else {
+      elementAt = myFixture.getFile().findElementAt(myFixture.getEditor().getCaretModel().getOffset());
+    }
+    assertNotNull(elementAt);
+
+    GoTypeOwner typeOwner = PsiTreeUtil.getNonStrictParentOfType(elementAt, GoTypeOwner.class);
+    assertNotNull("Cannot find type owner. Context element: " + elementAt.getText(), typeOwner);
+
+    GoType type = typeOwner.getGoType(null);
+    assertEquals(expectedTypeText, type == null ? "<unknown>" : type.getText());
+  }
+
+  private void doStatementTest(@NotNull String text, @NotNull String expectedTypeText) {
+    doTopLevelTest("func _() {\n" + text + "\n}", expectedTypeText);
+  }
+  
+  @Override
+  protected LightProjectDescriptor getProjectDescriptor() {
+    return createMockProjectDescriptor();
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    setUpProjectSdk();
+  }
 }
