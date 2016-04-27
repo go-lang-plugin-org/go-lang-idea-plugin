97e02d07bf2bcd9371c1133f364ed15ef9e518e6
Alexander Zolotov <goldifit@gmail.com>
2016-04-27 15:03:51 +0300

    Reimplement gobench output converter and add tests for it
    (cherry picked from commit 1979626a35af19e1ebf0764b66fa1ac3ccc75e8d)

--- src/com/goide/runconfig/testing/frameworks/gobench/GobenchEventsConverter.java
+++ src/com/goide/runconfig/testing/frameworks/gobench/GobenchEventsConverter.java
@@ -17,68 +17,128 @@
 package com.goide.runconfig.testing.frameworks.gobench;
 
 import com.goide.GoConstants;
+import com.goide.runconfig.testing.GoTestEventsConverterBase;
 import com.goide.runconfig.testing.GoTestLocator;
 import com.google.common.base.Stopwatch;
+import com.intellij.execution.process.ProcessOutputTypes;
 import com.intellij.execution.testframework.TestConsoleProperties;
+import com.intellij.execution.testframework.sm.ServiceMessageBuilder;
 import com.intellij.execution.testframework.sm.runner.OutputToGeneralTestEventsConverter;
-import com.intellij.execution.testframework.sm.runner.events.TestFailedEvent;
-import com.intellij.execution.testframework.sm.runner.events.TestFinishedEvent;
-import com.intellij.execution.testframework.sm.runner.events.TestStartedEvent;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.text.StringUtil;
+import jetbrains.buildServer.messages.serviceMessages.ServiceMessageTypes;
+import jetbrains.buildServer.messages.serviceMessages.ServiceMessageVisitor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.text.ParseException;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class GobenchEventsConverter extends OutputToGeneralTestEventsConverter {
+public class GobenchEventsConverter extends OutputToGeneralTestEventsConverter implements GoTestEventsConverterBase {
   private static final Pattern RUN = Pattern.compile("^(Benchmark" + GoConstants.IDENTIFIER_REGEX + ")");
-  private static final Pattern FAIL = Pattern.compile("^--- FAIL: (Benchmark" + GoConstants.IDENTIFIER_REGEX + ")");
-  private static final Pattern OK = Pattern.compile("^ok  \t");
+  private static final Pattern FAIL = Pattern.compile("^--- FAIL: (Benchmark" + GoConstants.IDENTIFIER_REGEX + ").*");
+  private static final Pattern TAIL = Pattern.compile("\\S*[ \t]+(.+)");
+  private static final String TOP_LEVEL_BENCHMARK_NAME = "<benchmark>";
 
   private final Stopwatch testStopwatch = Stopwatch.createUnstarted();
-  private String currentBenchmark = "<benchmark>";
+  private String currentBenchmark = TOP_LEVEL_BENCHMARK_NAME;
   private boolean benchmarkFailing;
+  private ServiceMessageVisitor myVisitor;
 
   public GobenchEventsConverter(TestConsoleProperties properties) {
     super(GobenchFramework.NAME, properties);
   }
 
   @Override
-  public void process(String text, Key outputType) {
+  public boolean processServiceMessages(@NotNull String text, Key outputType, ServiceMessageVisitor visitor) throws ParseException {
+    if (myVisitor == null && visitor != null) {
+      myVisitor = visitor;
+    }
+    if (text.isEmpty()) {
+      return true;
+    }
+    
     Matcher matcher;
-
-    if (OK.matcher(text).find()) {
-      maybeFinishCurrentTest();
-    }
-    else if ((matcher = RUN.matcher(text)).find()) {
-      maybeFinishCurrentTest();
+    if ((matcher = RUN.matcher(text)).find()) {
+      if (testStopwatch.isRunning()) {
+        finishCurrentTest(outputType, visitor);
+      }
       testStopwatch.start();
-      currentBenchmark = StringUtil.notNullize(matcher.group(1), "<benchmark>");
-      TestStartedEvent event = new TestStartedEvent(currentBenchmark, testUrl(currentBenchmark));
-      getProcessor().onTestStarted(event);
-    }
-    else if ((matcher = FAIL.matcher(text)).find()) {
-      currentBenchmark = StringUtil.notNullize(matcher.group(1), "<benchmark>");
-      benchmarkFailing = true;
+      currentBenchmark = StringUtil.notNullize(matcher.group(1), TOP_LEVEL_BENCHMARK_NAME);
+
+      String testStartedMessage = ServiceMessageBuilder.testStarted(currentBenchmark)
+        .addAttribute("locationHint", testUrl(currentBenchmark)).toString();
+      super.processServiceMessages(testStartedMessage, outputType, visitor);
+      processTailText(text, outputType, visitor, matcher.end(1));
+      return true;
     }
 
-    super.process(text, outputType);
+    if ((matcher = FAIL.matcher(text)).find()) {
+      String failedBenchmark = StringUtil.notNullize(matcher.group(1), TOP_LEVEL_BENCHMARK_NAME);
+      if (!failedBenchmark.equals(currentBenchmark)) {
+        finishCurrentTest(outputType, visitor);
+      }
+      currentBenchmark = failedBenchmark;
+      benchmarkFailing = true;
+      processTailText(text, outputType, visitor, matcher.end(1));
+      return true;
+    }
+
+    boolean isErrorMessage = ProcessOutputTypes.STDERR == outputType;
+    if (isTestStarted()) {
+      ServiceMessageBuilder builder = isErrorMessage ? ServiceMessageBuilder.testStdErr(currentBenchmark)
+                                                     : ServiceMessageBuilder.testStdOut(currentBenchmark);
+      super.processServiceMessages(builder.addAttribute("out", text).toString(), outputType, visitor);
+      return true;
+    }
+
+    ServiceMessageBuilder messageBuilder = new ServiceMessageBuilder(ServiceMessageTypes.MESSAGE);
+    if (isErrorMessage) {
+      messageBuilder.addAttribute("text", StringUtil.trimEnd(text, "\n")).addAttribute("status", "ERROR");
+    }
+    else {
+      messageBuilder.addAttribute("text", text).addAttribute("status", "NORMAL");
+    }
+    super.processServiceMessages(messageBuilder.toString(), outputType, visitor);
+    return true;
   }
 
-  private void maybeFinishCurrentTest() {
-    if (testStopwatch.isRunning()) {
+  @Override
+  public void flushBufferBeforeTerminating() {
+    try {
+      finishCurrentTest(null, myVisitor);
+    }
+    catch (ParseException ignore) {}
+    myVisitor = null;
+    super.flushBufferBeforeTerminating();
+  }
+
+  private boolean isTestStarted() {return !TOP_LEVEL_BENCHMARK_NAME.equals(currentBenchmark);}
+
+  private void processTailText(@NotNull String text, Key outputType, ServiceMessageVisitor visitor, int tailOffset)
+    throws ParseException {
+    String tailText = text.substring(tailOffset);
+    Matcher tailMatcher = TAIL.matcher(tailText);
+    if (!tailText.isEmpty() && tailMatcher.find())  {
+      processServiceMessages(tailText.substring(tailMatcher.start(1)), outputType, visitor);
+    }
+  }
+
+  private void finishCurrentTest(@Nullable Key outputType, @Nullable ServiceMessageVisitor visitor) throws ParseException {
+    if (isTestStarted()) {
       testStopwatch.stop();
       if (benchmarkFailing) {
-        TestFailedEvent event = new TestFailedEvent(currentBenchmark, "", null, true, null, null);
-        getProcessor().onTestFailure(event);
         benchmarkFailing = false;
+        String failedMessage = ServiceMessageBuilder.testFailed(currentBenchmark).addAttribute("message", "").toString();
+        super.processServiceMessages(failedMessage, outputType, visitor);
       }
 
-      TestFinishedEvent event = new TestFinishedEvent(currentBenchmark, testStopwatch.elapsed(TimeUnit.MILLISECONDS));
+      String finishedMessage = ServiceMessageBuilder.testFinished(currentBenchmark)
+        .addAttribute("duration", Long.toString(testStopwatch.elapsed(TimeUnit.MILLISECONDS))).toString();
       testStopwatch.reset();
-      getProcessor().onTestFinished(event);
+      super.processServiceMessages(finishedMessage, outputType, visitor);
     }
   }
 
---
+++ tests/com/goide/runconfig/testing/GobenchEventsConverterTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.goide.runconfig.testing;
+
+import com.goide.runconfig.testing.frameworks.gobench.GobenchFramework;
+import org.jetbrains.annotations.NotNull;
+
+public class GobenchEventsConverterTest extends GoEventsConverterTestCase {
+  public void testSuccessBenchmark() {
+    doTest();
+  }
+
+  public void testFailedBenchmark() {
+    doTest();
+  }
+  
+  public void testMixedBenchmark() {
+    doTest();
+  }
+  
+  public void testFailedCompilation() {
+    doTest();
+  }
+
+  @NotNull
+  @Override
+  protected String getBasePath() {
+    return "testing/gobench";
+  }
+  
+  @NotNull
+  @Override
+  protected GoTestFramework getTestFramework() {
+    return GobenchFramework.INSTANCE;
+  }
+}
