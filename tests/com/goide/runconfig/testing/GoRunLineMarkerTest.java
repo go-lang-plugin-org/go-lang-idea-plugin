53f64da1709cd8d7f9408e415a1c84a41d8c4400
Alexander Zolotov <goldifit@gmail.com>
2016-05-03 03:35:01 +0300

    [running] add run application gutter icons
    (cherry picked from commit 2a99bba6526426ed3e1c5e33ba2167b35ead13fc)

--- tests/com/goide/runconfig/testing/GoRunLineMarkerTest.java
+++ tests/com/goide/runconfig/testing/GoRunLineMarkerTest.java
@@ -13,6 +13,20 @@ public class GoRunLineMarkerTest extends GoRunConfigurationTestCase {
     assertEquals(4, myFixture.findAllGutters().size());
   }
 
+  public void testRunLineMater() {
+    myFixture.configureByText("a.go", "package main\n" +
+                                      "func m<caret>ain(){}");
+    assertEquals(1, myFixture.findGuttersAtCaret().size());
+    assertEquals(1, myFixture.findAllGutters().size());
+  }
+
+  public void testRunLineMarkerInNonMainFile() {
+    myFixture.configureByText("a.go", "package not_main\n" +
+                                      "func m<caret>ain(){}");
+    assertEquals(0, myFixture.findGuttersAtCaret().size());
+    assertEquals(0, myFixture.findAllGutters().size());
+  }
+
   @Override
   protected boolean isWriteActionRequired() {
     return false;
