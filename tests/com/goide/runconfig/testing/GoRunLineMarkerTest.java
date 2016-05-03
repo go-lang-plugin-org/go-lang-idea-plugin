de306e34e4d3982f192a93752c9ee6e27f8c48c6
Alexander Zolotov <goldifit@gmail.com>
2016-05-03 17:43:38 +0300

    [testing] add test for gocheck line markers
    (cherry picked from commit 671a01aeefb38dc2ee174a24a652eae392cd94b6)

--- tests/com/goide/runconfig/testing/GoRunLineMarkerTest.java
+++ tests/com/goide/runconfig/testing/GoRunLineMarkerTest.java
@@ -5,15 +5,19 @@ import com.goide.runconfig.GoRunConfigurationTestCase;
 public class GoRunLineMarkerTest extends GoRunConfigurationTestCase {
   public void testRunTestLineMarker() {
     myFixture.configureByText("a_test.go", "package m<caret>ain\n" +
+                                           "import .`gopkg.in/check.v1`\n" +
                                            "func TestName(){}\n" +
                                            "func BenchmarkName(){}\n" +
                                            "func ExampleName(){}\n" +
+                                           "type MySuite struct{}\n" +
+                                           "var _ = Suite(&MySuite{})\n" +
+                                           "func (s *MySuite) TestHelloWorld(c *C) {}\n" +
                                            "func Hello() {}");
     assertEquals(1, myFixture.findGuttersAtCaret().size());
-    assertEquals(4, myFixture.findAllGutters().size());
+    assertEquals(5, myFixture.findAllGutters().size());
   }
 
-  public void testRunLineMater() {
+  public void testRunLineMarker() {
     myFixture.configureByText("a.go", "package main\n" +
                                       "func m<caret>ain(){}");
     assertEquals(1, myFixture.findGuttersAtCaret().size());
