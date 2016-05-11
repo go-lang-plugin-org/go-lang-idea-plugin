616a3b49abd553c4c490843d54d9572878eaabd9
Alexander Zolotov <goldifit@gmail.com>
2016-05-11 19:43:36 +0300

    Fix test data
    (cherry picked from commit c098fc0bbcbaf0c16b42bdde7b6df054e9bf4d2b)

--- testData/inspections/go-defer-function-call/parens.go
+++ testData/inspections/go-defer-function-call/parens.go
@@ -1,5 +1,5 @@
 package main
 
 func _() {
-    defer <error descr="Expression in defer must be function call"><error descr="Expression in defer must not be parenthesized">(fu<caret>nc(){}())</error></error>
+    defer <error descr="Expression in defer must be function call"><error descr="Expression in defer must not be parenthesized">(<error descr="fu<caret>nc(){}() used as value">func(){}()</error>)</error></error>
 }
