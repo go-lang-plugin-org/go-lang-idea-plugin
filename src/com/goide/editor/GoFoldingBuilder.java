8a3d24f89efe9066eb7c5244376cda5fe5f03339
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2016-05-16 15:40:40 +0300

    fix NPE

--- src/com/goide/editor/GoFoldingBuilder.java
+++ src/com/goide/editor/GoFoldingBuilder.java
@@ -164,7 +164,7 @@ public class GoFoldingBuilder extends CustomFoldingBuilder implements DumbAware 
 
     for (GoCompositeLit compositeLit : PsiTreeUtil.findChildrenOfType(file, GoCompositeLit.class)) {
       GoLiteralValue literalValue = compositeLit.getLiteralValue();
-      if (literalValue.getElementList().size() > 1) {
+      if (literalValue != null && literalValue.getElementList().size() > 1) {
         fold(literalValue, literalValue.getLbrace(), literalValue.getRbrace(), "{...}", result);
       }
     }
