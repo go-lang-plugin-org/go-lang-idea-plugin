65829320d868fb8537161d83dde0bf69056cc138
Alexander Zolotov <goldifit@gmail.com>
2016-05-18 19:34:15 +0300

    fix tests in 143 branch

--- tests/com/goide/runconfig/testing/GoRunLineMarkerTest.java
+++ tests/com/goide/runconfig/testing/GoRunLineMarkerTest.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.goide.runconfig.testing;
 
 import com.goide.runconfig.GoRunConfigurationTestCase;
 
 public class GoRunLineMarkerTest extends GoRunConfigurationTestCase {
   public void testRunTestLineMarker() {
-    myFixture.configureByText("a_test.go", "package m<caret>ain\n" +
+    myFixture.configureByText("a_test.go", "package <caret>main\n" +
                                            "import .`gopkg.in/check.v1`\n" +
                                            "func TestName(){}\n" +
                                            "func BenchmarkName(){}\n" +
@@ -19,14 +35,14 @@ public class GoRunLineMarkerTest extends GoRunConfigurationTestCase {
 
   public void testRunLineMarker() {
     myFixture.configureByText("a.go", "package main\n" +
-                                      "func m<caret>ain(){}");
+                                      "func <caret>main(){}");
     assertEquals(1, myFixture.findGuttersAtCaret().size());
     assertEquals(1, myFixture.findAllGutters().size());
   }
 
   public void testRunLineMarkerInNonMainFile() {
     myFixture.configureByText("a.go", "package not_main\n" +
-                                      "func m<caret>ain(){}");
+                                      "func <caret>main(){}");
     assertEquals(0, myFixture.findGuttersAtCaret().size());
     assertEquals(0, myFixture.findAllGutters().size());
   }
