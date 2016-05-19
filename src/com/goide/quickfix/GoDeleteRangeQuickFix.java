5d7017b5a5dc8230955c674824c197568a869993
Sergey Ibragimov <sergey.ibragimov@jetbrains.com>
2016-05-19 19:19:37 +0300

    Add GoDeleteRangeQuickFix
    (cherry picked from commit 45a20a5087f5ec74c49616296f0f7892e480ca11)

---
+++ src/com/goide/quickfix/GoDeleteRangeQuickFix.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.goide.quickfix;
+
+import com.intellij.codeInspection.LocalQuickFixBase;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class GoDeleteRangeQuickFix extends LocalQuickFixBase {
+  private final Class<? extends PsiElement> myClazz;
+  private final IElementType myElementType;
+
+  public GoDeleteRangeQuickFix(@NotNull String name, @NotNull Class<? extends PsiElement> clazz) {
+    super(name);
+    myClazz = clazz;
+    myElementType = null;
+  }
+
+  public GoDeleteRangeQuickFix(@NotNull String name, @NotNull IElementType elementType) {
+    super(name);
+    myClazz = PsiElement.class;
+    myElementType = elementType;
+  }
+
+  //delete range include start,end
+  @Override
+  public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+    PsiElement start = descriptor.getStartElement();
+    PsiElement end = descriptor.getEndElement();
+    if (start != null && end != null && start.isValid() && end.isValid()) {
+      PsiElement parent = PsiTreeUtil.findCommonParent(start, end);
+      if (myClazz.isInstance(parent) && (myElementType == null || parent.getNode().getElementType() == myElementType)){
+        parent.getNode().removeRange(start.getNode(), end.getNode().getTreeNext());
+      }
+    }
+  }
+}
