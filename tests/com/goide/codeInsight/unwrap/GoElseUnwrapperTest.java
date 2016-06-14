f68a9831af5c8bf974948fbfe9b9229732170cb7
Sergey Ibragimov <sergey.ibragimov@jetbrains.com>
2016-06-14 13:51:36 +0300

    Add unwrappers  - if uwrapper  - else uwrapper  - else remover  - for uwrapper  - function argument uwrapper   - braces uwrapper
    (cherry picked from commit b60a5ecd630db6a8be9a025348a0297b8fb14eda)

---
+++ tests/com/goide/codeInsight/unwrap/GoElseUnwrapperTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.goide.codeInsight.unwrap;
+
+public class GoElseUnwrapperTest extends GoUnwrapTestCase {
+
+  public void testNoAction() {
+    assertUnwrapped("if true { \n}<caret>", "if true { \n}<caret>", 1);
+  }
+
+  public void testVoidElse() {
+    assertUnwrapped("if true {\n } else <caret> {  \n}\n", "if true {\n }\n", 1);
+  }
+
+  public void testElse() {
+    assertUnwrapped("if true {\n } else <caret> {  \n  var i int \n }\n", "if true {\n }\nvar i int\n", 1);
+  }
+
+  public void testElseWithTwoStatements() {
+    assertUnwrapped("if true {\n } else <caret> {  \nvar i int\n i = 23\n }\n", "if true {\n }\nvar i int\ni = 23\n", 1);
+  }
+
+  public void testElseIf() {
+    assertUnwrapped("if true {\n } else if <caret> {  \nvar i int\n i = 23\n }\n", "if true {\n }\nif {\n\tvar i int\n\ti = 23\n}\n", 1);
+  }
+}
---
+++ tests/com/goide/codeInsight/unwrap/GoForUnwrapperTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.goide.codeInsight.unwrap;
+
+public class GoForUnwrapperTest extends GoUnwrapTestCase {
+
+  public void testVoidFor() {
+    assertUnwrapped("for <caret> { \n}", "\n");
+  }
+
+  public void testForWithOneStatement() {
+    assertUnwrapped(
+      "for <caret> { \n" +
+      "var i int\n" +
+      "}", "var i int\n");
+  }
+
+  public void testForWithThreeStatements() {
+    assertUnwrapped(
+      "for <caret> { \n" +
+      "var a, b int\n" +
+      "a = 2\n" +
+      "b = 3\n" +
+      "}",
+      "var a, b int\n" +
+      "a = 2\n" +
+      "b = 3\n"
+    );
+  }
+
+  public void testForWithForClause() {
+    assertUnwrapped(
+      "for i := 1; i < 10; i++ { \n" +
+      "<caret>var a int\n" +
+      "a = i\n" +
+      "}",
+      "i := 1\n" +
+      "var a int\n" +
+      "a = i\n"
+    );
+  }
+
+  public void testForWithRangeClause() {
+    assertUnwrapped(
+      "for i := range \"asd\" { \n" +
+      "<caret>var a string\n" +
+      "a = i\n" +
+      "}",
+      "var a string\n" +
+      "a = i\n"
+    );
+  }
+}
