9540963725af7d51f09551f34f7dd1f6f4222b59
Sergey Ibragimov <sergey.ibragimov@jetbrains.com>
2016-06-16 17:24:42 +0300

    Move getSuggestionNames from GoIntroduceVariableBase to GoRefactoringUtil

--- src/com/goide/refactor/GoRefactoringUtil.java
+++ src/com/goide/refactor/GoRefactoringUtil.java
@@ -16,19 +16,19 @@
 
 package com.goide.refactor;
 
-import com.goide.psi.GoBlock;
-import com.goide.psi.GoStatement;
+import com.goide.psi.*;
 import com.goide.psi.impl.GoPsiImplUtil;
+import com.goide.psi.impl.GoTypeUtil;
 import com.intellij.codeInsight.PsiEquivalenceUtil;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiRecursiveElementVisitor;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.NameUtil;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class GoRefactoringUtil {
 
@@ -70,5 +70,70 @@ public class GoRefactoringUtil {
       statement = statement.getParent();
     }
     return statement == null ? GoPsiImplUtil.getTopLevelDeclaration(first) : statement;
+  }
+
+  public static LinkedHashSet<String> getSuggestedNames(GoExpression expression) {
+    return getSuggestedNames(expression, expression);
+  }
+
+  @NotNull
+  private static LinkedHashSet<String> getSuggestedNames(GoExpression expression, PsiElement context) {
+    // todo rewrite with names resolve; check occurrences contexts
+    if (expression.isEquivalentTo(context)) {
+      context = PsiTreeUtil.getParentOfType(context, GoBlock.class);
+    }
+    LinkedHashSet<String> usedNames = getNamesInContext(context);
+    LinkedHashSet<String> names = ContainerUtil.newLinkedHashSet();
+
+    if (expression instanceof GoCallExpr) {
+      GoReferenceExpression callReference = PsiTreeUtil.getChildOfType(expression, GoReferenceExpression.class);
+      if (callReference != null) {
+        String name = StringUtil.decapitalize(callReference.getIdentifier().getText());
+        for (String candidate : NameUtil.getSuggestionsByName(name, "", "", false, false, false)) {
+          if (!usedNames.contains(candidate)) names.add(candidate);
+        }
+      }
+    }
+
+    GoType type = expression.getGoType(null);
+    String typeText = GoPsiImplUtil.getText(type);
+    if (StringUtil.isNotEmpty(typeText)) {
+      boolean array = GoTypeUtil.isIterable(type) && !GoTypeUtil.isString(type);
+      for (String candidate : NameUtil.getSuggestionsByName(typeText, "", "", false, false, array)) {
+        if (!usedNames.contains(candidate)) {
+          names.add(candidate);
+        }
+      }
+    }
+
+    if (names.isEmpty()) {
+      if (usedNames.contains("i")) {
+        int counter = 1;
+        while (usedNames.contains("i" + counter)) counter++;
+        names.add("i" + counter);
+      }
+      else {
+        names.add("i");
+      }
+    }
+    return names;
+  }
+
+  @NotNull
+  private static LinkedHashSet<String> getNamesInContext(PsiElement context) {
+    if (context == null) return ContainerUtil.newLinkedHashSet();
+    LinkedHashSet<String> names = ContainerUtil.newLinkedHashSet();
+
+    for (GoNamedElement namedElement : PsiTreeUtil.findChildrenOfType(context, GoNamedElement.class)) {
+      names.add(namedElement.getName());
+    }
+    names.addAll(((GoFile)context.getContainingFile()).getImportMap().keySet());
+
+    GoFunctionDeclaration functionDeclaration = PsiTreeUtil.getParentOfType(context, GoFunctionDeclaration.class);
+    GoSignature signature = PsiTreeUtil.getChildOfType(functionDeclaration, GoSignature.class);
+    for (GoParamDefinition param : PsiTreeUtil.findChildrenOfType(signature, GoParamDefinition.class)) {
+      names.add(param.getName());
+    }
+    return names;
   }
 }
