f4ba0b655b505a5df406b72c3abc35a6a6abae16
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2016-06-18 07:31:02 +0300

    add tests for type conversions

--- testData/parser/Types.go
+++ testData/parser/Types.go
@@ -8,3 +8,22 @@ type (
   T4 func(int, float64) *T0
   T5 func(x int, y float64) *[]string
 )
+
+func main() {
+        type Point interface {
+	}
+
+	var p interface{} = nil
+	var c chan int = nil
+	var x func() = nil
+	var y func()int = nil
+	*Point(p)        // same as *(Point(p))
+	*(Point(p))
+	(*Point)(p)      // p is converted to *Point
+	<-chan int(c)    // same as <-(chan int(c))
+	(<-chan int)(c)  // c is converted to <-chan int
+	(func())(x)      // x is converted to func()
+	(func() int)(y)  // x is converted to func() int
+	func() int(y)    // x is converted to func() int (unambiguous)
+	//func()(x)        // function signature func() x
+}
