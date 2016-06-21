c4c7316e5f0f90c9502af5752207198690e9415e
Alexander Zolotov <goldifit@gmail.com>
2016-06-21 19:21:06 +0300

    @Override
    (cherry picked from commit 2f69ef1656570131cde840c2d580ff5ae4f7390b)

--- src/com/goide/psi/GoNamedElement.java
+++ src/com/goide/psi/GoNamedElement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,7 @@ public interface GoNamedElement extends GoCompositeElement, GoTypeOwner, PsiName
   @Nullable
   String getQualifiedName();
 
+  @Override
   @NotNull
   GoFile getContainingFile();
 
