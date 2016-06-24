fc79633de60a61b91617601b4fe476bd03997e88
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2016-06-24 23:39:08 +0300

    simplify lexer
    (cherry picked from commit 3e8baf5931691be5183874c143bc9b124cdc5f4c)

--- testData/lexer/escapedQuote.go
+++ testData/lexer/escapedQuote.go
@@ -14,3 +14,22 @@ const c = "\
 const c = "\n
 const c = "n
 const c = "
+
+'a'
+'ä'
+'本'
+'\t'
+'\000'
+'\007'
+'\377'
+'\x07'
+'\xff'
+'\u12e4'
+'\U00101234'
+
+'\''         // rune literal containing single quote character
+'aa'         // illegal: too many characters
+'\xa'        // illegal: too few hexadecimal digits
+'\0'         // illegal: too few octal digits
+'\uDFFF'     // illegal: surrogate half
+'\U00110000' // illegal: invalid Unicode code point
