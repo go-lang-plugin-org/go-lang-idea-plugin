b45293aa4fd90313f4e54dcc50f970cef7a11001
Sergey Ibragimov <sergey.ibragimov@jetbrains.com>
2016-06-27 13:43:02 +0300

    Add receiver names inspection (don't use generics names: me, this, self)
    (cherry picked from commit a85175d92c08a7e0d6d41e482412da3c419d5a78)

---
+++ src/com/goide/inspections/GoReceiverNamesInspection.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.goide.inspections;
+
+import com.goide.psi.GoReceiver;
+import com.goide.psi.GoVisitor;
+import com.goide.quickfix.GoRenameQuickFix;
+import com.intellij.codeInspection.LocalInspectionToolSession;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+
+public class GoReceiverNamesInspection extends GoInspectionBase {
+  private static String[] genericNames = {"me", "this", "self"};
+  private static HashSet<String> genericNamesSet = ContainerUtil.newHashSet(genericNames);
+
+  @NotNull
+  @Override
+  protected GoVisitor buildGoVisitor(@NotNull final ProblemsHolder holder, @NotNull LocalInspectionToolSession session) {
+    return new GoVisitor() {
+      @Override
+      public void visitReceiver(@NotNull GoReceiver o) {
+        if (genericNamesSet.contains(o.getName())) {
+          PsiElement identifier = o.getIdentifier();
+          if (identifier == null) return;
+          holder.registerProblem(identifier, "Receiver has generic name", new GoRenameQuickFix(o));
+        }
+      }
+    };
+  }
+}
