66d0b01692f51e54dd005f702fb5ef7bf45fed01
Alexander Zolotov <goldifit@gmail.com>
2016-07-04 22:03:22 +0300

    Simplify inspection
    (cherry picked from commit 0b959f4ad941de74f2840dd94851ce16933239b0)

--- src/com/goide/inspections/GoStructInitializationInspection.java
+++ src/com/goide/inspections/GoStructInitializationInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,20 +17,24 @@
 package com.goide.inspections;
 
 import com.goide.psi.*;
+import com.goide.psi.impl.GoPsiImplUtil;
 import com.goide.quickfix.GoReplaceWithNamedStructFieldQuickFix;
+import com.goide.util.GoUtil;
 import com.intellij.codeInspection.LocalInspectionToolSession;
+import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.codeInspection.ui.SingleCheckboxOptionsPanel;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.util.List;
 
 public class GoStructInitializationInspection extends GoInspectionBase {
-
-  @SuppressWarnings("WeakerAccess")
   public boolean reportImportedStructs;
 
   @NotNull
@@ -39,45 +43,12 @@ public class GoStructInitializationInspection extends GoInspectionBase {
     return new GoVisitor() {
       @Override
       public void visitCompositeLit(@NotNull GoCompositeLit o) {
-        super.visitCompositeLit(o);
-
         if (PsiTreeUtil.getParentOfType(o, GoReturnStatement.class, GoShortVarDeclaration.class, GoAssignmentStatement.class) == null) {
           return;
         }
-
-        if (!(o.getContainingFile() instanceof GoFile)) return;
-        String myPackage = ((GoFile)o.getContainingFile()).getPackageName();
-
-        List<GoFieldDeclaration> fields;
-        GoType type = o.getType();
-        if (type instanceof GoStructType) {
-          if (!canProcess(myPackage, (GoStructType)type)) return;
-          fields = ((GoStructType)type).getFieldDeclarationList();
-          if (hasAnonymousStructField(fields)) return;
-          processLiteralValue(holder, fields, o.getLiteralValue());
-          return;
-        }
-
-        GoTypeReferenceExpression ref = o.getTypeReferenceExpression();
-        if (ref == null || ref.resolveType() == null) return;
-        GoType refType = ref.resolveType().getUnderlyingType();
+        GoType refType = GoPsiImplUtil.getLiteralType(o);
         if (refType instanceof GoStructType) {
-          if (!canProcess(myPackage, (GoStructType)refType)) return;
-          fields = ((GoStructType)refType).getFieldDeclarationList();
-          if (hasAnonymousStructField(fields)) return;
-          processLiteralValue(holder, fields, o.getLiteralValue());
-        }
-        else if (refType instanceof GoArrayOrSliceType) {
-          type = ((GoArrayOrSliceType)refType).getType();
-          if (type == null || !(type.getUnderlyingType() instanceof GoStructType)) return;
-          if (!canProcess(myPackage, (GoStructType)type.getUnderlyingType())) return;
-          fields = ((GoStructType)type.getUnderlyingType()).getFieldDeclarationList();
-          if (hasAnonymousStructField(fields)) return;
-          for (GoElement element : o.getLiteralValue().getElementList()) {
-            GoValue value = element.getValue();
-            if (value == null || value.getExpression() != null || value.getLiteralValue() == null) continue;
-            processLiteralValue(holder, fields, value.getLiteralValue());
-          }
+          processStructType((GoStructType)refType, o, holder);
         }
       }
     };
@@ -86,12 +57,6 @@ public class GoStructInitializationInspection extends GoInspectionBase {
   @Override
   public JComponent createOptionsPanel() {
     return new SingleCheckboxOptionsPanel("Report for local type definitions as well", this, "reportImportedStructs");
-  }
-
-  private boolean canProcess(String myPackage, @NotNull GoStructType type) {
-    if (!(type.getContainingFile() instanceof GoFile)) return false;
-    String typePackage = ((GoFile)type.getContainingFile()).getPackageName();
-    return reportImportedStructs || myPackage == typePackage;
   }
 
   private static boolean hasAnonymousStructField(@NotNull List<GoFieldDeclaration> fields) {
@@ -103,25 +68,39 @@ public class GoStructInitializationInspection extends GoInspectionBase {
     return false;
   }
 
-  private static void processLiteralValue(@NotNull ProblemsHolder holder,
-                                          @NotNull List<GoFieldDeclaration> structFields,
-                                          @NotNull GoLiteralValue o) {
-    List<GoElement> elemList = o.getElementList();
-    for (int elemId = 0; elemId < elemList.size(); elemId++) {
-      GoElement element = elemList.get(elemId);
-      if (element.getKey() == null) {
-        String structFieldName = getFieldName(structFields.get(elemId));
-        holder.registerProblem(
-          element,
-          "Unnamed field initialization",
-          ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-          new GoReplaceWithNamedStructFieldQuickFix(structFieldName, element)
-        );
+  private void processStructType(@NotNull GoStructType structType,
+                                 @NotNull GoCompositeLit compositeLit,
+                                 @NotNull ProblemsHolder holder) {
+    if (reportImportedStructs || GoUtil.inSamePackage(structType.getContainingFile(), compositeLit.getContainingFile())) {
+      List<GoFieldDeclaration> fields = structType.getFieldDeclarationList();
+      if (!hasAnonymousStructField(fields)) {
+        processLiteralValue(holder, fields, compositeLit.getLiteralValue());
       }
     }
   }
 
+  private static void processLiteralValue(@NotNull ProblemsHolder holder,
+                                          @NotNull List<GoFieldDeclaration> structFields,
+                                          @Nullable GoLiteralValue o) {
+    if (o == null) return;
+    List<GoElement> elemList = o.getElementList();
+    for (int elemId = 0; elemId < elemList.size(); elemId++) {
+      ProgressManager.checkCanceled();
+      GoElement element = elemList.get(elemId);
+      if (element.getKey() == null && elemId < structFields.size()) {
+        String structFieldName = getFieldName(structFields.get(elemId));
+        LocalQuickFix[] fixes = structFieldName != null
+                                ? new LocalQuickFix[]{new GoReplaceWithNamedStructFieldQuickFix(structFieldName, element)}
+                                : LocalQuickFix.EMPTY_ARRAY;
+        holder.registerProblem(element, "Unnamed field initialization", ProblemHighlightType.GENERIC_ERROR_OR_WARNING, fixes);
+      }
+    }
+  }
+
+  @Nullable
   private static String getFieldName(@NotNull GoFieldDeclaration declaration) {
-    return declaration.getFieldDefinitionList().get(0).getIdentifier().getText();
+    List<GoFieldDefinition> list = declaration.getFieldDefinitionList();
+    GoFieldDefinition fieldDefinition = ContainerUtil.getFirstItem(list);
+    return fieldDefinition != null ? fieldDefinition.getIdentifier().getText() : null;
   }
 }
