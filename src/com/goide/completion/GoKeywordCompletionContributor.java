42bbcb46bf6566e80a1b328e59bd1eb2b62b3159
Alexander Zolotov <goldifit@gmail.com>
2016-07-04 22:40:48 +0300

    Complete type keyword in blocks

--- src/com/goide/completion/GoKeywordCompletionContributor.java
+++ src/com/goide/completion/GoKeywordCompletionContributor.java
@@ -48,7 +48,8 @@ public class GoKeywordCompletionContributor extends CompletionContributor implem
     extend(CompletionType.BASIC, importPattern(), new GoKeywordCompletionProvider(KEYWORD_PRIORITY, "import"));
     extend(CompletionType.BASIC, topLevelPattern(), new GoKeywordCompletionProvider(KEYWORD_PRIORITY, "const", "var", "func", "type"));
     extend(CompletionType.BASIC, insideBlockPattern(GoTypes.IDENTIFIER),
-           new GoKeywordCompletionProvider(KEYWORD_PRIORITY, "for", "const", "var", "return", "if", "switch", "go", "defer", "goto"));
+           new GoKeywordCompletionProvider(KEYWORD_PRIORITY, "type", "for", "const", "var", "return", "if", "switch", "go", "defer", 
+                                           "goto"));
     extend(CompletionType.BASIC, insideBlockPattern(GoTypes.IDENTIFIER),
            new GoKeywordCompletionProvider(KEYWORD_PRIORITY, EMPTY_INSERT_HANDLER, "fallthrough"));
     extend(CompletionType.BASIC, insideBlockPattern(GoTypes.IDENTIFIER),
--- tests/com/goide/completion/GoCompletionTest.java
+++ tests/com/goide/completion/GoCompletionTest.java
@@ -202,17 +202,17 @@ public class GoCompletionTest extends GoCompletionTestBase {
 
   public void testBlockKeywords() {
     myFixture.testCompletionVariants(getTestName(true) + ".go", "for", "const", "var", "return", "if", "switch", "go", "defer", "select",
-                                     "fallthrough", "goto", "main", "struct", "map");
+                                     "fallthrough", "goto", "main", "struct", "map", "type");
   }
 
   public void testBlockKeywordsInsideOneLineFunction() {
     myFixture.testCompletionVariants(getTestName(true) + ".go", "for", "const", "var", "return", "if", "switch", "go", "defer", "select",
-                                     "fallthrough", "goto", "main", "struct", "map");
+                                     "fallthrough", "goto", "main", "struct", "map", "type");
   }
 
   public void testBlockKeywordsInsideCaseStatement() {
     myFixture.testCompletionVariants(getTestName(true) + ".go", "for", "const", "var", "return", "if", "switch", "go", "defer", "select",
-                                     "fallthrough", "goto", "main", "struct", "map", "case", "default");
+                                     "fallthrough", "goto", "main", "struct", "map", "case", "default", "type");
   }
 
   public void testAddSpaceAfterKeyword() {
@@ -253,7 +253,7 @@ public class GoCompletionTest extends GoCompletionTestBase {
 
   public void testForStatementKeywords() {
     myFixture.testCompletionVariants(getTestName(true) + ".go", "bar", "break", "const", "continue", "defer", "for", "go", "if", "return",
-                                     "fallthrough", "goto", "select", "switch", "var", "struct", "map");
+                                     "fallthrough", "goto", "select", "switch", "var", "struct", "map", "type");
   }
 
   public void testDoNotCompleteKeywordsInsideConstSpec() {
