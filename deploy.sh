d1d2bc5393222df4cf23ddf86edb6b88b7f6ae8d
Alexander Zolotov <goldifit@gmail.com>
2016-07-09 19:49:40 +0300

    Updating site Sat Jul  9 19:49:40 MSK 2016

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ echo "\033[0;32mDeploying updates go GitHub...\033[0m"
 hugo
 message="Updating site `date`"
 if [ $# -eq 1 ]
-	then msg="$1"
+	then message="$1"
 fi
 
-git commit -am "$msg"
+git commit -am "$message"
 git push origin site
 git subtree push --prefix=public origin gh-pages
