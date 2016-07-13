35b2c9e7297038ad35711ebe6d5390a512dcac25
Alexander Zolotov <goldifit@gmail.com>
2016-07-14 01:05:24 +0300

    trying to fix brittle tests (cherry picked from commit 04c7602)

--- tests/com/goide/lexer/GoLexerTest.java
+++ tests/com/goide/lexer/GoLexerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package com.goide.lexer;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.testFramework.LexerTestCase;
 import org.jetbrains.annotations.NotNull;
 
@@ -48,7 +49,7 @@ public class GoLexerTest extends LexerTestCase {
 
   private void doTest() {
     try {
-      String text = FileUtil.loadFile(new File("./" + PATH + "/" + getTestName(true) + ".go"));
+      String text = FileUtil.loadFile(new File("./" + PATH + "/" + getTestName(true) + ".go"), CharsetToolkit.UTF8);
       String actual = printTokens(StringUtil.convertLineSeparators(text.trim()), 0);
       assertSameLinesWithFile(new File(PATH + "/" + getTestName(true) + ".txt").getAbsolutePath(), actual);
     } catch (IOException e) {
