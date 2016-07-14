6f62fffa8f5fbc795d6fca2cd70c584f359c263d
Florin Patan <florinpatan@gmail.com>
2016-07-14 21:48:57 +0100

    Direct assignments to struct fields are allowed when the struct is a pointer (fixes #2655)
    (cherry picked from commit f8c45374c2926359f2eec0145dc431fbb5274ea0)

--- testData/highlighting/assignToStructFieldInMap.go
+++ testData/highlighting/assignToStructFieldInMap.go
@@ -4,6 +4,21 @@ type data struct {
 	name string
 }
 
+func _() {
+	type foo struct{ Name string }
+
+	bad := map[string]foo{}
+
+	for key := range bad {
+		<error descr="cannot assign to bad[key].Name">bad[key].Name</error> = bad[key].Name + " Updated"
+	}
+
+	good := map[string]*foo{}
+	for key := range good {
+		good[key].Name = good[key].Name + " Updated"
+	}
+}
+
 func main() {
 	m := map[string]data{"x": {"one"}}
 	<error descr="cannot assign to m[\"x\"].name">m["x"].name</error> = "two"
