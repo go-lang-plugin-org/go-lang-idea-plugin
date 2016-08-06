c4f486219a4ad8c228b558c885fa987c80c41813
Alexander Zolotov <goldifit@gmail.com>
2016-08-07 02:58:49 +0300

    Regenerate parser

--- gen/com/goide/psi/impl/GoChannelTypeImpl.java
+++ gen/com/goide/psi/impl/GoChannelTypeImpl.java
@@ -25,6 +25,7 @@ import com.intellij.psi.PsiElementVisitor;
 import com.goide.psi.GoPsiTreeUtil;
 import static com.goide.GoTypes.*;
 import com.goide.psi.*;
+import com.goide.stubs.GoTypeStub;
 import com.intellij.psi.stubs.IStubElementType;
 
 public class GoChannelTypeImpl extends GoTypeImpl implements GoChannelType {
