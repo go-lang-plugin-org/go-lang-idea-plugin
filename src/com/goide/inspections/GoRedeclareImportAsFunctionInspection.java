291e1f05b33c958fbf8e71166d0a84cfe90de4ec
Florin Patan <florinpatan@gmail.com>
2016-08-09 02:36:00 +0300

    Fix underscore used as name inspections (#fixes 2716) (cherry picked from commit c17f713)

--- src/com/goide/inspections/GoRedeclareImportAsFunctionInspection.java
+++ src/com/goide/inspections/GoRedeclareImportAsFunctionInspection.java
@@ -32,7 +32,9 @@ public class GoRedeclareImportAsFunctionInspection extends GoInspectionBase {
       @Override
       public void visitFunctionDeclaration(@NotNull GoFunctionDeclaration o) {
         String functionName = o.getName();
-        if (StringUtil.isNotEmpty(functionName) && o.getContainingFile().getImportMap().containsKey(functionName)) {
+        if (StringUtil.isNotEmpty(functionName) &&
+            !"_".equals(functionName) &&
+            o.getContainingFile().getImportMap().containsKey(functionName)) {
           holder.registerProblem(o.getIdentifier(), "import \"" + functionName + "\" redeclared in this block", new GoRenameQuickFix(o));
         }
       }
